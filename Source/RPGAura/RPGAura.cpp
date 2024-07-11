50d7c61c2db3403ed62337c24e0e4140b2201f05
my-laptop <2655667253@qq.com>
2024-07-11 17:16:19 +0800

    初始化项目

---
+++ Source/RPGAura/RPGAura.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RPGAura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPGAura, "RPGAura" );
