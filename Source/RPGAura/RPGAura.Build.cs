baf5e08fdfab3d2ec361965a7d68d80cc3a20716
my-laptop <2655667253@qq.com>
2024-07-11 18:23:43 +0800

    添加:基础角色类

--- Source/RPGAura/RPGAura.Build.cs
+++ Source/RPGAura/RPGAura.Build.cs
@@ -7,14 +7,16 @@ public class RPGAura : ModuleRules
 	public RPGAura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] { "RPGAura/Characters" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
