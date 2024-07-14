464eab28db1e38971171e256a8fa17ce175dd0e2
my-laptop <2655667253@qq.com>
2024-07-14 18:44:44 +0800

    添加:添加EffectActor 测试访问器效果

---
+++ Source/RPGAura/Private/Dev/EffectActor.cpp
@@ -0,0 +1,61 @@
+// Copyright GGBAO 
+
+
+#include "Dev/EffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "Components/SphereComponent.h"
+#include "GAS/AttributeSet/BaseAttributeSet.h"
+
+DEFINE_LOG_CATEGORY_STATIC(AEffectActorLog, All, All);
+
+AEffectActor::AEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SetRootComponent(StaticMeshComponent);
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(GetRootComponent());
+	SphereComponent->InitSphereRadius(250.0f);
+
+}
+
+void AEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AEffectActor::OnBeginOverBegin);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &AEffectActor::OnEndOverBegin);
+}
+
+void AEffectActor::OnBeginOverBegin(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+                                    UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                    const FHitResult &SweepResult)
+{
+
+	const auto AbilitySystem = Cast<IAbilitySystemInterface>(OtherActor);
+	if (!AbilitySystem) { return; }
+
+	const auto AbilitySystemComponent = AbilitySystem->GetAbilitySystemComponent();
+	if (!AbilitySystemComponent) { return; }
+	
+	
+	const auto MyAttributeSet = Cast<UBaseAttributeSet>(
+		AbilitySystemComponent->GetAttributeSet(UBaseAttributeSet::StaticClass()));
+	if (!MyAttributeSet) { return; }
+
+
+	// TODO 暂时使用const_cast来强制更改,以后用GamePlay Effects 来修改
+	auto MutableAs = const_cast<UBaseAttributeSet *>(MyAttributeSet);
+
+	if (!MutableAs) { return; }
+	MutableAs->SetCurrentHealth(MyAttributeSet->GetCurrentHealth() + (-10.0f));
+
+	UE_LOG(AEffectActorLog, Warning, TEXT("Effect"));
+
+}
+
+void AEffectActor::OnEndOverBegin(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+                                  UPrimitiveComponent *OtherComp, int32 OtherBodyIndex) {}
---
+++ Source/RPGAura/Public/Dev/EffectActor.h
@@ -0,0 +1,39 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EffectActor.generated.h"
+
+/**
+* 
+*/
+
+class USphereComponent;
+
+UCLASS()
+class RPGAURA_API AEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEffectActor();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+	UFUNCTION()
+	void OnBeginOverBegin(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp,
+	                      int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnEndOverBegin(UPrimitiveComponent*OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
+
