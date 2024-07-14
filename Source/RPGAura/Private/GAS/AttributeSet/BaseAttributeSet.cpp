df4fdff3001e20249818a2148b0e631663e41603
my-laptop <2655667253@qq.com>
2024-07-14 15:42:50 +0800

    添加:给GAS的AS添加4个新属性

--- Source/RPGAura/Private/GAS/AttributeSet/BaseAttributeSet.cpp
+++ Source/RPGAura/Private/GAS/AttributeSet/BaseAttributeSet.cpp
@@ -3,4 +3,41 @@
 
 #include "GAS/AttributeSet/BaseAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
 UBaseAttributeSet::UBaseAttributeSet() {}
+
+void UBaseAttributeSet::OnRep_CurrentHealth(const FGameplayAttributeData &OldHealth) const
+{
+	// 通知GAS currentHealth刚刚被复制了,使GAS整个系统协调工作,让能力系统记录这一变化并保存旧值的跟踪,以防止需要回滚
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, CurrentHealth, OldHealth);
+}
+
+void UBaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData &OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UBaseAttributeSet::OnRep_CurrentMana(const FGameplayAttributeData &OldCurrentMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, CurrentMana, OldCurrentMana);
+}
+
+void UBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData &OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 注册health变量用于复制,这是复制任何变量所需的步骤
+	// 复制条件 COND_None ,也就是没有条件的复制
+	// REPNOTIFY_Always 意味着如果在服务器上设置了值,就复制给客户端,那个值将被更新和设置,这里的值就是我们的CurrentHealth
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, CurrentHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, CurrentMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
--- Source/RPGAura/Public/GAS/AttributeSet/BaseAttributeSet.h
+++ Source/RPGAura/Public/GAS/AttributeSet/BaseAttributeSet.h
@@ -16,4 +16,36 @@ class RPGAURA_API UBaseAttributeSet : public UAttributeSet
 
 public:
 	UBaseAttributeSet();
+
+	// CurrentHealth属性
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentHealth, BlueprintReadOnly, Category = "Hp")
+	FGameplayAttributeData CurrentHealth;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, BlueprintReadOnly, Category = "Hp")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentMana, BlueprintReadOnly, Category = "Mp")
+	FGameplayAttributeData CurrentMana;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, BlueprintReadOnly, Category = "Mp")
+	FGameplayAttributeData MaxMana;
+
+	/// CurrentHealth变量复制到客户端的时候,就会调用该函数
+	/// 该函数可以接受1个参数,但是必须也是FGameplayAttributeData类型的
+	/// 当有一个参数时,它将是旧值作为参数
+	UFUNCTION()
+	void OnRep_CurrentHealth(const FGameplayAttributeData &OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData &OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_CurrentMana(const FGameplayAttributeData &OldCurrentMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData &OldMaxMana) const;
+
+	/// 设置 当前类 中的属性在网络中如何进行复制
+	/// @param OutLifetimeProps 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const override;
 };
