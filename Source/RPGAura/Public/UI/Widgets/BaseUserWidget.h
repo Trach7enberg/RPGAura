a1bdb916e9307627ef37ceb0e9dd9b3469ca01b2
my-laptop <2655667253@qq.com>
2024-07-30 23:32:10 +0800

    添加:为UBaseUserWidget新增一个游戏标签变量

--- Source/RPGAura/Public/UI/Widgets/BaseUserWidget.h
+++ Source/RPGAura/Public/UI/Widgets/BaseUserWidget.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Blueprint/UserWidget.h"
 #include "BaseUserWidget.generated.h"
 
@@ -16,8 +17,21 @@ class RPGAURA_API UBaseUserWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
+	FGameplayTag GetCurrentGameplayTag() const
+	{
+		return CurrentGameplayTag;
+	}
+
+	void SetCurrentGameplayTag(const FGameplayTag &NewGameplayTag)
+	{
+		this->CurrentGameplayTag = NewGameplayTag;
+	}
+
 	UFUNCTION(BlueprintCallable, Category="WidgetController")
-	UBaseWidgetController *GetWidgetController() const { return WidgetController; }
+	UBaseWidgetController *GetWidgetController() const
+	{
+		return WidgetController;
+	}
 
 	UFUNCTION(BlueprintCallable, Category="WidgetController")
 	void SetWidgetController(UBaseWidgetController *NewWidgetController);
@@ -26,8 +40,11 @@ protected:
 	virtual void NativeOnInitialized() override;
 
 	/// 当控制器被设置时调用该事件
-	UFUNCTION(BlueprintImplementableEvent, Category="WidgetController")
+	UFUNCTION(BlueprintImplementableEvent, Category="Widget|WidgetController")
 	void WidgetControllerSet();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Widget|WidgetGameplayTag")
+	FGameplayTag CurrentGameplayTag;
 
 private:
 	TObjectPtr<UBaseWidgetController> WidgetController;
