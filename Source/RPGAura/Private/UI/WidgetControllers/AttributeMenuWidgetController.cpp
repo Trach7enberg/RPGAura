a67a3de4caecfa5d6047c637d01e9f2e5cf743b7
my-laptop <2655667253@qq.com>
2024-07-30 23:28:13 +0800

    添加:给UAttributeMenuWidgetController新增函数用于当属性变化的时候广播该属性,使得属性菜单能正确接收广播的值来显示变化

--- Source/RPGAura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -3,12 +3,185 @@
 
 #include "UI/WidgetControllers/AttributeMenuWidgetController.h"
 
+#include "CoreTypes/RPGAuraGameplayTags.h"
+#include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
+#include "GAS/AttributeSet/BaseAttributeSet.h"
+#include "GAS/Data/AttributeInfo.h"
+
+DEFINE_LOG_CATEGORY_STATIC(UAttributeMenuWidgetControllerLog, All, All);
+
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
-	
+	if (!AttributeInfo)
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("映射数据资产表信息不能为nullptr!"));
+		return;
+	}
+
+	if (!IsWidgetControllerParamsValid())
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
+		return;
+	}
+
+	const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
+	if (!MyAs)
+	{
+		return;
+	}
+
+	TArray<FGameplayTag *> TagsArray;
+	FRPGAuraGameplayTags::Get().GetAllGameplayTags(TagsArray);
+	UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("ALL:%d"), TagsArray.Num());
+
+
+	TArray<FGameplayAttribute> Array;
+	GetWidgetControllerParams().CurrentAbilitySystemComponent->GetAllAttributes(Array);
+	for (auto &Attribute : Array)
+	{
+		const auto AttributeInfoStruct = AttributeInfo->GetAttributeInfoByAttributeName(Attribute.GetName());
+
+		if (!AttributeInfoStruct)
+		{
+			UE_LOG(UAttributeMenuWidgetControllerLog, Warning, TEXT("找不到[%s]对应的属性信息结构体"),*Attribute.GetName());
+			continue;
+		}
+		
+		AttributeInfoStruct->AttributeValue = Attribute.GetNumericValue(MyAs);
+		OnAttributeChanged.Broadcast(*AttributeInfoStruct);
+		// UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("%s"), *AttributeInfoStruct->AttributeName.ToString());
+
+	}
+
+
 }
 
 void UAttributeMenuWidgetController::BindCallBack()
 {
-	
+	if (!IsWidgetControllerParamsValid())
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
+		return;
+	}
+
+	const auto Asc = Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
+	if (!Asc)
+	{
+		return;
+	}
+
+	const auto MyAsc = Cast<UBaseAbilitySystemComponent>(Asc);
+	if (!MyAsc)
+	{
+		return;
+	}
+
+	const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
+	if (!MyAs)
+	{
+		return;
+	}
+
+	{
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+								   GetGameplayAttributeValueChangeDelegate(MyAs->GetStrengthAttribute())
+								   .AddLambda([this](const FOnAttributeChangeData &Data)
+								   {
+									   HandleAnyAttributeChange(Data);
+								   });
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetIntelligenceAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetResilienceAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetVigorAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+	}
+
+	{
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetArmorAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetArmorPenetrationAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetBlockChanceAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetCriticalHitChanceAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetCriticalHitResistanceAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetCriticalHitDamageAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetHealthRegenerationAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetManaRegenerationAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetMaxHealthAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+		GetWidgetControllerParams().CurrentAbilitySystemComponent->
+									GetGameplayAttributeValueChangeDelegate(MyAs->GetMaxManaAttribute())
+									.AddLambda([this](const FOnAttributeChangeData &Data)
+									{
+										HandleAnyAttributeChange(Data);
+									});
+	}
+}
+
+void UAttributeMenuWidgetController::HandleAnyAttributeChange(const FOnAttributeChangeData &Data) const
+{
+	const auto AttributeInfoStruct = AttributeInfo->GetAttributeInfoByAttributeName(
+		Data.Attribute.GetName());
+	if (!AttributeInfoStruct)
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Warning, TEXT("映射属性信息结构无效!"));
+		return;
+	}
+	AttributeInfoStruct->AttributeValue = Data.NewValue;
+	OnAttributeChanged.Broadcast(*AttributeInfoStruct);
 }
--- Source/RPGAura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
+++ Source/RPGAura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
@@ -6,8 +6,13 @@
 #include "UI/WidgetControllers/BaseWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+struct FOnAttributeChangeData;
+class UAttributeInfo;
+struct FRPGAuraAttributeInfo;
+
+
 /**
- * 
+ *  属性菜单的控制器类
  */
 UCLASS()
 class RPGAURA_API UAttributeMenuWidgetController : public UBaseWidgetController
@@ -17,4 +22,23 @@ class RPGAURA_API UAttributeMenuWidgetController : public UBaseWidgetController
 public:
 	virtual void BroadcastInitialValues() override;
 	virtual void BindCallBack() override;
+
+	/// 属性值变化时的委托
+	UPROPERTY(BlueprintAssignable, Category="GAS | Attributes")
+	FOnAttributeChangedSignature OnAttributeChanged;
+
+	/// 游戏标签到属性的映射数据资产表信息
+	/// @return 映射数据资产表信息
+	TObjectPtr<UAttributeInfo> GetAttributeInfoAsset() const
+	{
+		return AttributeInfo;
+	}
+
+protected:
+	/// 游戏标签到属性的映射数据资产表信息
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<UAttributeInfo> AttributeInfo = nullptr;
+
+private:
+	void HandleAnyAttributeChange(const FOnAttributeChangeData &Data) const;
 };
