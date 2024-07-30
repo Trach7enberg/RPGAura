e7b19899a0ea572595a87649b8453ca8e2cf2607
my-laptop <2655667253@qq.com>
2024-07-30 23:25:14 +0800

    添加:给UAttributeInfo新增函数GetAttributeInfoByAttributeName

--- Source/RPGAura/Private/GAS/Data/AttributeInfo.cpp
+++ Source/RPGAura/Private/GAS/Data/AttributeInfo.cpp
@@ -3,17 +3,20 @@
 
 #include "GAS/Data/AttributeInfo.h"
 
+#include "CoreTypes/RPGAuraGameplayTags.h"
+
 DEFINE_LOG_CATEGORY_STATIC(UAttributeInfoLog, All, All);
 
-FRPGAuraAttributeInfo UAttributeInfo::GetAttributeInfoByTag(const FGameplayTag &AttributeTag,
-                                                            const bool BLogNotFound) const
+FRPGAuraAttributeInfo *UAttributeInfo::GetAttributeInfoByTag(const FGameplayTag &AttributeTag,
+                                                             const bool BLogNotFound)
 {
 
-	for (const auto AttributeInfo : RPGAuraAttributeInfos)
+	for (auto &AttributeInfo : RPGAuraAttributeInfos)
 	{
+
 		if (AttributeInfo.AttributeTag == AttributeTag)
 		{
-			return AttributeInfo;
+			return &AttributeInfo;
 		}
 	}
 
@@ -21,5 +24,16 @@ FRPGAuraAttributeInfo UAttributeInfo::GetAttributeInfoByTag(const FGameplayTag &
 	{
 		UE_LOG(UAttributeInfoLog, Warning, TEXT("找不到 [%s] 对应的属性信息"), *AttributeTag.ToString());
 	}
-	return FRPGAuraAttributeInfo{};
+	return nullptr;
+}
+
+FRPGAuraAttributeInfo *UAttributeInfo::GetAttributeInfoByAttributeName(const FString &AttributeName, bool BLogNotFound)
+{
+	const auto Tag = FRPGAuraGameplayTags::GetGameplayTagByAttributeName(AttributeName);
+	if (!Tag)
+	{
+		UE_LOG(UAttributeInfoLog, Warning, TEXT("找不到对应属性的GT"));
+		return nullptr;
+	}
+	return GetAttributeInfoByTag(*Tag);
 }
--- Source/RPGAura/Public/GAS/Data/AttributeInfo.h
+++ Source/RPGAura/Public/GAS/Data/AttributeInfo.h
@@ -3,32 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameplayTagContainer.h"
+#include "CoreTypes/RPGAuraCoreTypes.h"
 #include "Engine/DataAsset.h"
 #include "AttributeInfo.generated.h"
 
-/// 用于广播GAS属性的结构体
-USTRUCT(BlueprintType)
-struct FRPGAuraAttributeInfo
-{
-	GENERATED_BODY()
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FGameplayTag AttributeTag = FGameplayTag();
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FText AttributeName = FText();
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FText AttributeDescription = FText();
-
-	UPROPERTY(BlueprintReadOnly)
-	float AttributeValue = 0.f;
-
-};
 
 /**
- * 存储着游戏标签到属性的映射数据资产表
+ * 存储着游戏标签到属性的映射数据资产表类
  */
 UCLASS()
 class RPGAURA_API UAttributeInfo : public UDataAsset
@@ -39,9 +21,17 @@ public:
 	/// 通过GT返回对应的属性信息
 	/// @param AttributeTag 属性标签
 	/// @param BLogNotFound 没找到时是否打印日志
-	/// @return 属性信息结构体
-	FRPGAuraAttributeInfo GetAttributeInfoByTag(const FGameplayTag &AttributeTag, bool BLogNotFound = true) const;
+	/// @return 属性信息结构体,没找到则为nullptr
+	FRPGAuraAttributeInfo* GetAttributeInfoByTag(const FGameplayTag &AttributeTag, bool BLogNotFound = true) ;
 
+	/// 通过属性集的相应属性名字返回对应的属性信息
+	/// @param AttributeName 属性名字
+	/// @param BLogNotFound 没找到时是否打印日志
+	/// @return 属性信息结构体,没找到则为nullptr
+	FRPGAuraAttributeInfo* GetAttributeInfoByAttributeName(const FString &AttributeName, bool BLogNotFound = true) ;
+	
+	
+	
 	/// 在蓝图中填充的数据数组,游戏标签到属性的映射
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TArray<FRPGAuraAttributeInfo> RPGAuraAttributeInfos;
