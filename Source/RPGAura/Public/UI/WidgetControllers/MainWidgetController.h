9fa238297faf301fa387a30818ab3f15869affdd
my-laptop <2655667253@qq.com>
2024-07-30 23:33:44 +0800

    修改:挪动UMainWidgetController类下的结构和委托声明到CoreTypes.h

--- Source/RPGAura/Public/UI/WidgetControllers/MainWidgetController.h
+++ Source/RPGAura/Public/UI/WidgetControllers/MainWidgetController.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameplayTagContainer.h"
 #include "UI/WidgetControllers/BaseWidgetController.h"
 #include "MainWidgetController.generated.h"
 
@@ -12,35 +11,7 @@ struct FGameplayTag;
 struct FGameplayTagContainer;
 struct FOnAttributeChangeData;
 
-/// 用于显示当角色应用GE时的一个结构体
-USTRUCT(BlueprintType)
-struct FUIWidgetRow : public FTableRowBase
-{
-	GENERATED_BODY()
 
-	// 要显示的标签
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-	FGameplayTag Tag = FGameplayTag();
-
-	// 要显示的消息
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-	FText Message = FText();
-
-	// 要显示的UI
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-	TSubclassOf<UBaseUserWidget> MessageWidget;
-
-	// 要显示的图片
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-	UTexture2D *Image = nullptr;
-
-};
-
-/// 给属性变化时的委托
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAttributeChangedSignature, float, NewValue,
-                                             bool, /* NewValue增加还是减少,增加则为true */ BIsIncreased);
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMessageWidgetRowSignature, const FUIWidgetRow, Row);
 
 /**
  * 主要widget的控制器类
@@ -53,19 +24,19 @@ class RPGAURA_API UMainWidgetController : public UBaseWidgetController
 public:
 	/// 广播GAS中的生命值变化,bool为true时候表明生命在增加
 	UPROPERTY(BlueprintAssignable, Category="GAS | Attributes")
-	FOnAttributeChangedSignature OnHealthChangedSignature;
+	FOnVitalAttributeChangedSignature OnHealthChangedSignature;
 
 	/// 广播GAS中的最大生命值变化,bool为true时候表明最大生命在增加
 	UPROPERTY(BlueprintAssignable, Category="GAS | Attributes")
-	FOnAttributeChangedSignature OnMaxHealthChangedSignature;
+	FOnVitalAttributeChangedSignature OnMaxHealthChangedSignature;
 
 	/// 广播GAS中的魔力值变化,bool为true时候表明魔力值在增加
 	UPROPERTY(BlueprintAssignable, Category="GAS | Attributes")
-	FOnAttributeChangedSignature OnManaChangedSignature;
+	FOnVitalAttributeChangedSignature OnManaChangedSignature;
 
 	/// 广播GAS中的最大魔力值变化,bool为true时候表明最大魔力值在增加
 	UPROPERTY(BlueprintAssignable, Category="GAS | Attributes")
-	FOnAttributeChangedSignature OnMaxManaChangedSignature;
+	FOnVitalAttributeChangedSignature OnMaxManaChangedSignature;
 
 	/// 广播 数据表中的表行,表行的类型为FUIWidgetRow
 	UPROPERTY(BlueprintAssignable, Category="GAS | Messages")
