94ba43e3ca53e824d29cb4ff44688e21f91bf3a2
my-laptop <2655667253@qq.com>
2024-07-30 23:42:35 +0800

    添加:给FRPGAuraGameplayTags类新增4个私有数组,分别存着对应类型的标签指针,其中AllGameplayTagsArray存着全部游戏标签的指针,新增函数GetGameplayTagByAttributeName、GetGameplayTagsByType

--- Source/RPGAura/Private/CoreTypes/RPGAuraGameplayTags.cpp
+++ Source/RPGAura/Private/CoreTypes/RPGAuraGameplayTags.cpp
@@ -5,39 +5,152 @@
 
 #include "GameplayTagsManager.h"
 
+DEFINE_LOG_CATEGORY_STATIC(FRPGAuraGameplayTagsLog, All, All);
+
 FRPGAuraGameplayTags FRPGAuraGameplayTags::GameplayTags;
+TArray<FGameplayTag*> FRPGAuraGameplayTags::VitalGameplayTagsArray;
+TArray<FGameplayTag*> FRPGAuraGameplayTags::PrimaryGameplayTagsArray;
+TArray<FGameplayTag*> FRPGAuraGameplayTags::SecondaryGameplayTagsArray;
+TArray<FGameplayTag*> FRPGAuraGameplayTags::AllGameplayTagsArray;;
 
 void FRPGAuraGameplayTags::InitGameplayTags()
 {
+    GameplayTags.Attribute_Vital_CurrentHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Vital.CurrentHealth");
+    GameplayTags.Attribute_Vital_CurrentMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Vital.CurrentMana");
 
-	GameplayTags.Attribute_Secondary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Primary.Strength");
-	GameplayTags.Attribute_Secondary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Primary.Intelligence");
-	GameplayTags.Attribute_Secondary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Primary.Resilience");
-	GameplayTags.Attribute_Secondary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Primary.Vigor");
+    GameplayTags.Attribute_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Primary.Strength");
+    GameplayTags.Attribute_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Primary.Intelligence");
+    GameplayTags.Attribute_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Primary.Resilience");
+    GameplayTags.Attribute_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Primary.Vigor");
 
-	GameplayTags.Attribute_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.Armor",UTF8TEXT("减少伤害，提高格挡几率"));
-	GameplayTags.Attribute_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.ArmorPenetration");
-	GameplayTags.Attribute_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.BlockChance");
-	GameplayTags.Attribute_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.CriticalHitChance");
-	GameplayTags.Attribute_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.CriticalHitResistance");
-	GameplayTags.Attribute_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.CriticalHitDamage");
-	GameplayTags.Attribute_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.HealthRegeneration");
-	GameplayTags.Attribute_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.ManaRegeneration");
-	GameplayTags.Attribute_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.MaxHealth");
-	GameplayTags.Attribute_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		"Attributes.Secondary.MaxMana");
+    GameplayTags.Attribute_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.Armor",
+        UTF8TEXT("减少伤害，提高格挡几率"));
+    GameplayTags.Attribute_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.ArmorPenetration");
+    GameplayTags.Attribute_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.BlockChance");
+    GameplayTags.Attribute_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.CriticalHitChance");
+    GameplayTags.Attribute_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.CriticalHitResistance");
+    GameplayTags.Attribute_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.CriticalHitDamage");
+    GameplayTags.Attribute_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.HealthRegeneration");
+    GameplayTags.Attribute_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.ManaRegeneration");
+    GameplayTags.Attribute_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.MaxHealth");
+    GameplayTags.Attribute_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+        "Attributes.Secondary.MaxMana");
 
+    // 添加到Vital数组
+    GameplayTags.VitalGameplayTagsArray.Add(&GameplayTags.Attribute_Vital_CurrentHealth);
+    GameplayTags.VitalGameplayTagsArray.Add(&GameplayTags.Attribute_Vital_CurrentMana);
+
+    // 添加到Primary数组
+    GameplayTags.PrimaryGameplayTagsArray.Add(&(GameplayTags.Attribute_Primary_Strength));
+    GameplayTags.PrimaryGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Intelligence);
+    GameplayTags.PrimaryGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Resilience);
+    GameplayTags.PrimaryGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Vigor);
+
+    // 添加到Secondary数组
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_Armor);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_ArmorPenetration);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_BlockChance);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitChance);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitResistance);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitDamage);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_HealthRegeneration);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_ManaRegeneration);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_MaxHealth);
+    GameplayTags.SecondaryGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_MaxMana);
+
+    // 添加标签到all数组
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Vital_CurrentHealth);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Vital_CurrentMana);
+
+    GameplayTags.AllGameplayTagsArray.Add(&(GameplayTags.Attribute_Primary_Strength));
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Intelligence);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Resilience);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Primary_Vigor);
+
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_Armor);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_ArmorPenetration);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_BlockChance);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitChance);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitResistance);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_CriticalHitDamage);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_HealthRegeneration);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_ManaRegeneration);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_MaxHealth);
+    GameplayTags.AllGameplayTagsArray.Add(&GameplayTags.Attribute_Secondary_MaxMana);
+    // 添加标签到all数组
+}
+
+void FRPGAuraGameplayTags::GetAllGameplayTags(TArray<FGameplayTag*>& Array)
+{
+    Array = AllGameplayTagsArray;
+}
+
+FGameplayTag* FRPGAuraGameplayTags::GetGameplayTagByAttributeName(const FString& AttributeName,
+                                                                  const EGameplayTagType TagType)
+{
+    switch (TagType)
+    {
+        case EGameplayTagType::AllGameplayTags:
+            return GetGameplayTagByAttributeName(AttributeName, AllGameplayTagsArray);
+        case EGameplayTagType::VitalGameplayTags:
+            return GetGameplayTagByAttributeName(AttributeName, VitalGameplayTagsArray);
+
+        case EGameplayTagType::PrimaryGameplayTags:
+            return GetGameplayTagByAttributeName(AttributeName, PrimaryGameplayTagsArray);
+
+        case EGameplayTagType::SecondaryGameplayTags:
+            return GetGameplayTagByAttributeName(AttributeName, SecondaryGameplayTagsArray);
+
+        default:
+            return nullptr;
+    }
+}
+
+TArray<FGameplayTag*>* FRPGAuraGameplayTags::GetGameplayTagsByType(const EGameplayTagType TagType)
+{
+    switch (TagType)
+    {
+        case EGameplayTagType::AllGameplayTags:
+            return &AllGameplayTagsArray;
+            break;
+        case EGameplayTagType::VitalGameplayTags:
+            return &VitalGameplayTagsArray;
+            break;
+        case EGameplayTagType::PrimaryGameplayTags:
+            return &PrimaryGameplayTagsArray;
+            break;
+        case EGameplayTagType::SecondaryGameplayTags:
+            return &SecondaryGameplayTagsArray;
+            break;
+    }
+
+    return nullptr;
+}
+
+FGameplayTag* FRPGAuraGameplayTags::GetGameplayTagByAttributeName(const FString& AttributeName,
+                                                                  const TArray<FGameplayTag*>& Array)
+{
+    for (const auto GameplayTag : Array)
+    {
+        if (GameplayTag->ToString().Contains(AttributeName))
+        {
+            return GameplayTag;
+        }
+    }
+    return nullptr;
 }
--- Source/RPGAura/Public/CoreTypes/RPGAuraGameplayTags.h
+++ Source/RPGAura/Public/CoreTypes/RPGAuraGameplayTags.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
+#include "RPGAuraCoreTypes.h"
 
 /**
  * 包含当前项目GameplayTags的结构体
@@ -11,32 +12,64 @@
 // USTRUCT(BlueprintType)
 struct FRPGAuraGameplayTags
 {
-	// GENERATED_BODY()
-	
-	static const FRPGAuraGameplayTags &Get()
-	{
-		return GameplayTags;
-	}
+    // GENERATED_BODY()
 
-	/// 添加NativeGameplayTag,native意思是在Cpp中,即在Cpp中添加GameplayTag
-	static void InitGameplayTags();
+    /// 添加NativeGameplayTag,native意思是在Cpp中,即在Cpp中添加GameplayTag
+    static void InitGameplayTags();
 
-	FGameplayTag Attribute_Secondary_Strength;
-	FGameplayTag Attribute_Secondary_Intelligence;
-	FGameplayTag Attribute_Secondary_Resilience;
-	FGameplayTag Attribute_Secondary_Vigor;
+    FGameplayTag Attribute_Vital_CurrentHealth;
+    FGameplayTag Attribute_Vital_CurrentMana;
 
-	FGameplayTag Attribute_Secondary_Armor;
-	FGameplayTag Attribute_Secondary_ArmorPenetration;
-	FGameplayTag Attribute_Secondary_BlockChance;
-	FGameplayTag Attribute_Secondary_CriticalHitChance;
-	FGameplayTag Attribute_Secondary_CriticalHitResistance;
-	FGameplayTag Attribute_Secondary_CriticalHitDamage;
-	FGameplayTag Attribute_Secondary_HealthRegeneration;
-	FGameplayTag Attribute_Secondary_ManaRegeneration;
-	FGameplayTag Attribute_Secondary_MaxHealth;
-	FGameplayTag Attribute_Secondary_MaxMana;
+    FGameplayTag Attribute_Primary_Strength;
+    FGameplayTag Attribute_Primary_Intelligence;
+    FGameplayTag Attribute_Primary_Resilience;
+    FGameplayTag Attribute_Primary_Vigor;
+
+    FGameplayTag Attribute_Secondary_Armor;
+    FGameplayTag Attribute_Secondary_ArmorPenetration;
+    FGameplayTag Attribute_Secondary_BlockChance;
+    FGameplayTag Attribute_Secondary_CriticalHitChance;
+    FGameplayTag Attribute_Secondary_CriticalHitResistance;
+    FGameplayTag Attribute_Secondary_CriticalHitDamage;
+    FGameplayTag Attribute_Secondary_HealthRegeneration;
+    FGameplayTag Attribute_Secondary_ManaRegeneration;
+    FGameplayTag Attribute_Secondary_MaxHealth;
+    FGameplayTag Attribute_Secondary_MaxMana;
+
+    /// 获取GameplayTags静态实例
+    /// @return GameplayTags单例
+    static const FRPGAuraGameplayTags& Get()
+    {
+        return GameplayTags;
+    }
+
+    /// 获取所有GameplayTag
+    /// @param Array 存储数据的数组
+    static void GetAllGameplayTags(TArray<FGameplayTag*>& Array);
+
+    /// 根据属性集属性的名字和标签的类型(全部、重要、主要、次要)获取GameplayTag
+    /// @param AttributeName 属性名字
+    /// @param TagType 标签类型
+    /// @return GameplayTag 
+    static FGameplayTag* GetGameplayTagByAttributeName(const FString& AttributeName,
+                                                       EGameplayTagType TagType =
+                                                           EGameplayTagType::AllGameplayTags);
+
+
+    /// 根据游戏标签的类型(全部、重要、主要、次要)返回对应的标签数组
+    /// @param TagType 游戏标签的类型(Vital、Primary、Secondary、还是所有标签)
+    /// @return 游戏标签数组的指针
+    static TArray<FGameplayTag*>* GetGameplayTagsByType(EGameplayTagType TagType);
 
 private:
-	static FRPGAuraGameplayTags GameplayTags;
+    /// 存储所有游戏标签的静态类的实例
+    static FRPGAuraGameplayTags GameplayTags;
+
+    static TArray<FGameplayTag*> VitalGameplayTagsArray;
+    static TArray<FGameplayTag*> PrimaryGameplayTagsArray;
+    static TArray<FGameplayTag*> SecondaryGameplayTagsArray;
+    static TArray<FGameplayTag*> AllGameplayTagsArray;
+
+    static FGameplayTag* GetGameplayTagByAttributeName(const FString& AttributeName,
+                                                       const TArray<FGameplayTag*>& Array);
 };
