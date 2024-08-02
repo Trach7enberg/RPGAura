3cc1e6b09283a88c337e365ed2db3228e67c815d
my-laptop <2655667253@qq.com>
2024-08-03 01:39:17 +0800

    添加:给能力类新增一个初始的默认能力标签,用于和从输入传来的标签进行比较,相等就激活能力

--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseGameplayAbility.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseGameplayAbility.h
@@ -12,6 +12,10 @@
 UCLASS()
 class RPGAURA_API UBaseGameplayAbility : public UGameplayAbility
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    // 每一个能力都有的初始默认能力标签
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="StartUp")
+    FGameplayTag StartUpTag;
 };
