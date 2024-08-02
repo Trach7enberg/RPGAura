d3568e0f96d9d99f0f46176d0e376d103d8533fe
my-laptop <2655667253@qq.com>
2024-08-03 01:42:23 +0800

    添加:输入动作映射到游戏标签的数据资产类

--- Source/RPGAura/Private/Input/DataAssets/RPGAuraInputConfig.cpp
+++ Source/RPGAura/Private/Input/DataAssets/RPGAuraInputConfig.cpp
@@ -3,3 +3,20 @@
 
 #include "Input/DataAssets/RPGAuraInputConfig.h"
 
+DEFINE_LOG_CATEGORY_STATIC(URPGAuraInputConfigLog, All, All);
+
+const UInputAction* URPGAuraInputConfig::FindInputAction(const FGameplayTag& InputTag, const bool BlogNotFound) const
+{
+    for (const auto& InputToGameplayTagStruct : AbilityInputActions)
+    {
+        if (InputToGameplayTagStruct.InputTag == InputTag && InputToGameplayTagStruct.InputAction)
+        {
+            return InputToGameplayTagStruct.InputAction;
+        }
+    }
+    if (BlogNotFound)
+    {
+        UE_LOG(URPGAuraInputConfigLog, Warning, TEXT("没找到 InputTag %s 对应的InputAction!"), *InputTag.ToString());
+    }
+    return nullptr;
+}
--- Source/RPGAura/Public/Input/DataAssets/RPGAuraInputConfig.h
+++ Source/RPGAura/Public/Input/DataAssets/RPGAuraInputConfig.h
@@ -3,15 +3,40 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Engine/DataAsset.h"
 #include "RPGAuraInputConfig.generated.h"
 
+USTRUCT(BlueprintType)
+struct FRPGAuraInputToGameplayTag
+{
+    GENERATED_BODY()
+
+
+    UPROPERTY(EditDefaultsOnly, meta=(description="输入动作"))
+    const class UInputAction* InputAction = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, meta=(description="输入动作对应的游戏标签"))
+     FGameplayTag InputTag = FGameplayTag();
+};
+
 /**
- * 
+ *  输入动作映射到游戏标签的数据资产类
  */
 UCLASS()
 class RPGAURA_API URPGAuraInputConfig : public UDataAsset
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    TArray<FRPGAuraInputToGameplayTag> AbilityInputActions;
+
+    /// 根据游戏标签查找输入动作(InputAction)
+    /// @param InputTag 游戏标签
+    /// @param BlogNotFound 是否打印
+    /// @return 找到的输入动作 
+    const UInputAction* FindInputAction(const FGameplayTag& InputTag, bool BlogNotFound = true) const;
 };
+
+
