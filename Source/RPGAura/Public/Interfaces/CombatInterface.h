5e96ac35a6c49eae9b64521dbe7b63adca5ff7dc
my-laptop <2655667253@qq.com>
2024-08-05 23:50:25 +0800

    添加:战斗接口新增一个函数,用于获取武器尖端Socket的location

--- Source/RPGAura/Public/Interfaces/CombatInterface.h
+++ Source/RPGAura/Public/Interfaces/CombatInterface.h
@@ -10,7 +10,7 @@
 UINTERFACE(MinimalAPI)
 class UCombatInterface : public UInterface
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 };
 
 /**
@@ -18,10 +18,14 @@ class UCombatInterface : public UInterface
  */
 class RPGAURA_API ICombatInterface
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	/// 获取当前角色的等级
-	/// @return 等级
-	virtual int32 GetCharacterLevel() = 0;
+    /// 获取当前角色的等级
+    /// @return 等级
+    virtual int32 GetCharacterLevel() = 0;
+
+    /// 获取武器攻击的socket位置(比如子弹、火球发射的位置)
+    /// @return 
+    virtual FVector GetCombatSocketLocation() = 0;
 };
