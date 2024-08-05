e369a1f1f07812fdbfa7453f65f6c891f560619b
my-laptop <2655667253@qq.com>
2024-08-05 23:42:37 +0800

    修改:UAttributeInfo,采用自动读取标签填充数组

--- Source/RPGAura/Private/GAS/Data/AttributeInfo.cpp
+++ Source/RPGAura/Private/GAS/Data/AttributeInfo.cpp
@@ -4,21 +4,34 @@
 #include "GAS/Data/AttributeInfo.h"
 
 #include "CoreTypes/RPGAuraGameplayTags.h"
-
 DEFINE_LOG_CATEGORY_STATIC(UAttributeInfoLog, All, All);
+
+UAttributeInfo::UAttributeInfo()
+{
+    for (const auto GameplayTag : FRPGAuraGameplayTags::GetGameplayTagsArray())
+    {
+        FRPGAuraAttributeInfo Tmp;
+        Tmp.AttributeTag = *GameplayTag;
+        RPGAuraAttributeInfos.Add(Tmp);
+    }
+}
 
 FRPGAuraAttributeInfo UAttributeInfo::FindAttributeInfoByTag(const FGameplayTag& AttributeTag)
 {
-    for (auto AttributeInfo : RPGAuraAttributeInfos)
+    if (!this )
+    {
+        UE_LOG(UAttributeInfoLog, Error, TEXT("致命错误"));
+        return FRPGAuraAttributeInfo{};
+    }
+
+    for (const auto& AttributeInfo : RPGAuraAttributeInfos)
     {
         if (AttributeInfo.AttributeTag.MatchesTag(AttributeTag))
         {
             return AttributeInfo;
-            
         }
     }
 
     UE_LOG(UAttributeInfoLog, Warning, TEXT("找不到 [%s] 对应的属性信息"), *AttributeTag.ToString());
     return FRPGAuraAttributeInfo{};
 }
-
--- Source/RPGAura/Public/GAS/Data/AttributeInfo.h
+++ Source/RPGAura/Public/GAS/Data/AttributeInfo.h
@@ -3,10 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "CoreTypes/RPGAuraCoreTypes.h"
 #include "Engine/DataAsset.h"
 #include "AttributeInfo.generated.h"
-
 
 
 /**
@@ -15,19 +15,20 @@
 UCLASS()
 class RPGAURA_API UAttributeInfo : public UDataAsset
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	/// 通过GT返回对应的属性信息
-	/// @param AttributeTag 属性标签
-	/// @return 属性信息结构体,没找到则为nullptr
-	FRPGAuraAttributeInfo FindAttributeInfoByTag(const FGameplayTag &AttributeTag) ;
+    UAttributeInfo();
+    /// 通过GT返回对应的属性信息
+    /// @param AttributeTag 属性标签
+    /// @return 属性信息结构体,没找到则为nullptr
+    FRPGAuraAttributeInfo FindAttributeInfoByTag(const FGameplayTag& AttributeTag);
 
-	
-	
-	
-	
-	/// 在蓝图中填充的数据数组,游戏标签到属性的映射
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TArray<FRPGAuraAttributeInfo> RPGAuraAttributeInfos;
+
+    /// 在蓝图中填充的数据数组,游戏标签到属性的映射
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TArray<FRPGAuraAttributeInfo> RPGAuraAttributeInfos;
+
+    
+
 };
