5d1c4c93c10bb4fb2f27d447e64ed11de851c486
my-laptop <2655667253@qq.com>
2024-08-05 23:48:01 +0800

    添加:魔法射弹能力(Ability)类,

---
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseProjectileSpell.cpp
@@ -0,0 +1,70 @@
+// Copyright GGBAO 
+
+
+#include "GAS/GameplayAbilities/BaseProjectileSpell.h"
+
+#include "Interfaces/CombatInterface.h"
+#include "Weapons/Projectiles/BaseProjectile.h"
+
+DEFINE_LOG_CATEGORY_STATIC(UBaseProjectileSpellLog, All, All);
+
+void UBaseProjectileSpell::SpawnProjectile() const
+{
+    // 飞弹能力(投射物)仅在服务器上生成
+    if (!GetAvatarActorFromActorInfo()->HasAuthority())
+    {
+        return;
+    }
+
+    if (!ProjectileClass)
+    {
+        UE_LOG(UBaseProjectileSpellLog, Error, TEXT("Projectile Class不能为nullptr!"));
+        return;
+    }
+    if (!GetOwningActorFromActorInfo() || !GetAvatarActorFromActorInfo())
+    {
+        UE_LOG(UBaseProjectileSpellLog, Error, TEXT("Actor信息获取失败!"));
+        return;
+    }
+
+    // Instigate : 引发者就是引发我们正在进行的事情的角色 , 必须是Pawn
+    const auto Instigate = Cast<APawn>(GetAvatarActorFromActorInfo());
+    if (!Instigate)
+    {
+        return;
+    }
+
+    const auto CombatInter = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+    if (!CombatInter)
+    {
+        UE_LOG(UBaseProjectileSpellLog, Error, TEXT("转换失败!"));
+        return;
+    }
+
+    // TODO 设置飞弹的Rotation
+    FTransform Transform;
+    Transform.SetLocation(CombatInter->GetCombatSocketLocation());
+
+    const auto Projectile = GetWorld()->SpawnActorDeferred<ABaseProjectile>(
+        ProjectileClass,
+        Transform,
+        GetOwningActorFromActorInfo(),
+        Instigate,
+        ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+    // TODO 给飞弹一个GameplayEffectSpec , 可以让它在overlap的时候应用
+
+    Projectile->FinishSpawning(Transform);
+}
+
+void UBaseProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    // 飞弹能力(投射物)仅在服务器上生成
+    if (!HasAuthority(&ActivationInfo))
+    {
+        return;
+    }
+    // SpawnProjectile();
+}
---
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseProjectileSpell.h
@@ -0,0 +1,29 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/GameplayAbilities/BaseGameplayAbility.h"
+#include "BaseProjectileSpell.generated.h"
+
+class ABaseProjectile;
+/**
+ * 基础投射魔法能力类
+ */
+UCLASS()
+class RPGAURA_API UBaseProjectileSpell : public UBaseGameplayAbility
+{
+    GENERATED_BODY()
+
+public:
+    /// 生成投射物
+    UFUNCTION(BlueprintCallable, Category="Spawn")
+    void SpawnProjectile() const;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Class")
+    TSubclassOf<ABaseProjectile> ProjectileClass;
+
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
