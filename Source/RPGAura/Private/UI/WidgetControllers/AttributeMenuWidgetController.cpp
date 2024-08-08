76a1fbb955f26a91ab21229668786f8a6730f2e5
my-laptop <2655667253@qq.com>
2024-08-08 23:40:34 +0800

    修改:为BroadcastAttributeInfoStruct函数新增判断

--- Source/RPGAura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -11,92 +11,85 @@
 DEFINE_LOG_CATEGORY_STATIC(UAttributeMenuWidgetControllerLog, All, All);
 
 
-UAttributeMenuWidgetController::UAttributeMenuWidgetController()
-{
-}
+UAttributeMenuWidgetController::UAttributeMenuWidgetController() {}
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
-    if (!AttributeInfo)
-    {
-        UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("映射数据资产表信息不能为nullptr!"));
-        return;
-    }
+	if (!AttributeInfo)
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("映射数据资产表信息不能为nullptr!"));
+		return;
+	}
 
-    if (!IsWidgetControllerParamsValid())
-    {
-        UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
-        return;
-    }
+	if (!IsWidgetControllerParamsValid())
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
+		return;
+	}
 
-    const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
-    if (!MyAs)
-    {
-        return;
-    }
+	const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
+	if (!MyAs) { return; }
 
-    const auto MyAcs = Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
-    if (!MyAcs)
-    {
-        return;
-    }
+	const auto MyAcs = Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
+	if (!MyAcs) { return; }
 
-    // UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("ALL:%d"), FRPGAuraGameplayTags::GetGameplayTagsMap().Num());
-    // UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("ALL:%d"), MyAs->TagToAttributeMap.Num());
+	// UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("ALL:%d"), FRPGAuraGameplayTags::GetGameplayTagsMap().Num());
+	// UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("ALL:%d"), MyAs->TagToAttributeMap.Num());
 
-    // 遍历属性集的主要和次要属性,广播属性信息
-    // DoFirstBroadcast(MyAs, FRPGAuraGameplayTags::PrimaryGameplayTagsArray);
-    // DoFirstBroadcast(MyAs, FRPGAuraGameplayTags::SecondaryGameplayTagsArray);
-    for (const auto& Pair : MyAs->TagToAttributeMap)
-    {
-        BroadcastAttributeInfoStruct(MyAs, Pair.Key, Pair.Value());
-    }
+	// 遍历属性集的主要和次要属性,广播属性信息
+	// DoFirstBroadcast(MyAs, FRPGAuraGameplayTags::PrimaryGameplayTagsArray);
+	// DoFirstBroadcast(MyAs, FRPGAuraGameplayTags::SecondaryGameplayTagsArray);
+	for (const auto& Pair : MyAs->TagToAttributeMap) { BroadcastAttributeInfoStruct(MyAs, Pair.Key, Pair.Value()); }
 }
 
 void UAttributeMenuWidgetController::BindCallBack()
 {
-    if (!IsWidgetControllerParamsValid())
-    {
-        UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
-        return;
-    }
+	if (!IsWidgetControllerParamsValid())
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Error, TEXT("控制器的基本参数无效!"));
+		return;
+	}
 
-    const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
-    if (!MyAs)
-    {
-        return;
-    }
+	const auto MyAs = Cast<UBaseAttributeSet>(GetWidgetControllerParams().CurrentAttributeSet);
+	if (!MyAs) { return; }
 
-    for (const auto& Pair : MyAs->TagToAttributeMap)
-    {
-        HandleAnyAttributeChange(MyAs, Pair.Key, Pair.Value());
-    }
+	for (const auto& Pair : MyAs->TagToAttributeMap) { HandleAnyAttributeChange(MyAs, Pair.Key, Pair.Value()); }
 }
 
 
-void UAttributeMenuWidgetController::HandleAnyAttributeChange(const UBaseAttributeSet* MyAs, const FGameplayTag& GameplayTag, const FGameplayAttribute& Attribute)
+void UAttributeMenuWidgetController::HandleAnyAttributeChange(const UBaseAttributeSet* MyAs,
+                                                              const FGameplayTag& GameplayTag,
+                                                              const FGameplayAttribute& Attribute)
 {
-    GetWidgetControllerParams().CurrentAbilitySystemComponent->
-                                GetGameplayAttributeValueChangeDelegate(Attribute)
-                                .AddLambda([this,MyAs, GameplayTag, Attribute](const FOnAttributeChangeData& Data)
-                                {
-                                    BroadcastAttributeInfoStruct(MyAs, GameplayTag, Attribute);
-                                });
+	GetWidgetControllerParams().CurrentAbilitySystemComponent->
+	                            GetGameplayAttributeValueChangeDelegate(Attribute)
+	                            .AddLambda([this,MyAs, GameplayTag, Attribute](const FOnAttributeChangeData& Data)
+	                            {
+		                            BroadcastAttributeInfoStruct(MyAs, GameplayTag, Attribute);
+	                            });
 }
 
 
-void UAttributeMenuWidgetController::BroadcastAttributeInfoStruct(const UBaseAttributeSet* MyAs, const FGameplayTag& GameplayTag, const FGameplayAttribute& Attribute)
+void UAttributeMenuWidgetController::BroadcastAttributeInfoStruct(const UBaseAttributeSet* MyAs,
+                                                                  const FGameplayTag& GameplayTag,
+                                                                  const FGameplayAttribute& Attribute)
 {
+	if (!AttributeInfo)
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Warning, TEXT("致命错误"));
+		return;
+	}
+	auto AttributeInfoStruct = AttributeInfo->FindAttributeInfoByTag(GameplayTag);
+	// FRPGAuraAttributeInfo{};
 
-    auto AttributeInfoStruct = AttributeInfo->FindAttributeInfoByTag(GameplayTag);
-    // FRPGAuraAttributeInfo{};
+	if (!AttributeInfoStruct.AttributeTag.IsValid())
+	{
+		UE_LOG(UAttributeMenuWidgetControllerLog, Warning, TEXT("找不到GT:[%s]对应的属性信息结构体"), *GameplayTag.ToString());
+		return;
+	}
 
-    if (!AttributeInfoStruct.AttributeTag.IsValid())
-    {
-        UE_LOG(UAttributeMenuWidgetControllerLog, Warning, TEXT("找不到GT:[%s]对应的属性信息结构体"), *GameplayTag.ToString());
-        return;
-    }
+	AttributeInfoStruct.AttributeValue = Attribute.GetNumericValue(MyAs);
 
-    AttributeInfoStruct.AttributeValue = Attribute.GetNumericValue(MyAs);
-    OnAttributeChanged.Broadcast(AttributeInfoStruct);
+	// TODO Bug待修复,多人模式下一广播就报错Crash,和在WBP_TextValueRow里绑定的回调函数通知属性变化有关系
+	OnAttributeChanged.Broadcast(AttributeInfoStruct);
 }
--- Source/RPGAura/Private/UI/Widgets/AttributeMenuWidget.cpp
+++ Source/RPGAura/Private/UI/Widgets/AttributeMenuWidget.cpp
@@ -6,120 +6,121 @@
 #include "AbilitySystemComponent.h"
 #include "Components/VerticalBox.h"
 #include "CoreTypes/RPGAuraGameplayTags.h"
+#include "FunctionLibrary/WidgetControllerBpFuncLib.h"
 #include "UI/WidgetControllers/AttributeMenuWidgetController.h"
 
 DEFINE_LOG_CATEGORY_STATIC(UAttributeMenuWidgetLog, All, All);
 
 void UAttributeMenuWidget::InitVerticalArea()
 {
-    if (!TextValueRowButtonWidgetClass || !TextValueRowWidgetClass || !GetOwningPlayer())
-    {
-        UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("致命错误!"))
-        return;
-    }
+	if (!TextValueRowButtonWidgetClass || !TextValueRowWidgetClass || !GetOwningPlayer())
+	{
+		UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("致命错误!"))
+		return;
+	}
 
-    CurrentAttributePointArea->ClearChildren();
+	CurrentAttributePointArea->ClearChildren();
 
-    // 创建可加属性点数区域
-    CurrentAttributePointRow = CreateWidget<UBaseUserWidget>(GetOwningPlayer(), TextValueRowWidgetClass);
-    CurrentAttributePointArea->AddChild(CurrentAttributePointRow);
+	// 创建可加属性点数区域
+	CurrentAttributePointRow = CreateWidget<UBaseUserWidget>(GetOwningPlayer(), TextValueRowWidgetClass);
+	CurrentAttributePointArea->AddChild(CurrentAttributePointRow);
 
-    // 创建主要属性widget
-    CreateVerticalAreaWidgets(FRPGAuraGameplayTags::
-                              GetGameplayTagsByType(EGameplayTagType::PrimaryGameplayTags),
-                              EGameplayTagType::PrimaryGameplayTags,
-                              VerticalBox_PrimaryAttriArea);
+	// 创建主要属性widget
+	CreateVerticalAreaWidgets(FRPGAuraGameplayTags::
+	                          GetGameplayTagsByType(EGameplayTagType::PrimaryGameplayTags),
+	                          EGameplayTagType::PrimaryGameplayTags,
+	                          VerticalBox_PrimaryAttriArea);
 
-    // 创建次要属性widget
-    VerticalBox_SecondaryAttriArea->ClearChildren();
-    CreateVerticalAreaWidgets(FRPGAuraGameplayTags::
-                              GetGameplayTagsByType(EGameplayTagType::SecondaryGameplayTags),
-                              EGameplayTagType::SecondaryGameplayTags,
-                              VerticalBox_SecondaryAttriArea);
+	// 创建次要属性widget
+	VerticalBox_SecondaryAttriArea->ClearChildren();
+	CreateVerticalAreaWidgets(FRPGAuraGameplayTags::
+	                          GetGameplayTagsByType(EGameplayTagType::SecondaryGameplayTags),
+	                          EGameplayTagType::SecondaryGameplayTags,
+	                          VerticalBox_SecondaryAttriArea);
+
+	if (!GetWidgetController())
+	{
+		SetWidgetController(UWidgetControllerBpFuncLib::GetAttributeMenuWidgetController(GetOwningPlayer()));
+	}
+	GetWidgetController()->BroadcastInitialValues();
 }
 
 UBaseUserWidget* UAttributeMenuWidget::GetWidgetByGameplayTag(const EGameplayTagType GameplayTagType,
                                                               const FGameplayTag Tag)
 {
-    UBaseUserWidget* Result = nullptr;
-    switch (GameplayTagType)
-    {
-        case EGameplayTagType::AllGameplayTags:
-            Result = FindWidgetByTagFromArray(AllAttriAreaWidgetArray, Tag);
-            break;
-        case EGameplayTagType::VitalGameplayTags:
-            Result = FindWidgetByTagFromArray(VitalAttriAreaWidgetArray, Tag);
-            break;
-        case EGameplayTagType::PrimaryGameplayTags:
-            Result = FindWidgetByTagFromArray(PrimaryAttriAreaWidgetArray, Tag);
-            break;
-        case EGameplayTagType::SecondaryGameplayTags:
-            Result = FindWidgetByTagFromArray(SecondaryAttriAreaWidgetArray, Tag);
-            break;
-        default: ;
-    }
-    return Result;
+	UBaseUserWidget* Result = nullptr;
+	switch (GameplayTagType)
+	{
+	case EGameplayTagType::AllGameplayTags:
+		Result = FindWidgetByTagFromArray(AllAttriAreaWidgetArray, Tag);
+		break;
+	case EGameplayTagType::VitalGameplayTags:
+		Result = FindWidgetByTagFromArray(VitalAttriAreaWidgetArray, Tag);
+		break;
+	case EGameplayTagType::PrimaryGameplayTags:
+		Result = FindWidgetByTagFromArray(PrimaryAttriAreaWidgetArray, Tag);
+		break;
+	case EGameplayTagType::SecondaryGameplayTags:
+		Result = FindWidgetByTagFromArray(SecondaryAttriAreaWidgetArray, Tag);
+		break;
+	default: ;
+	}
+	return Result;
 }
 
 void UAttributeMenuWidget::NativeConstruct()
 {
-    Super::NativeConstruct();
-    // InitVerticalArea();
-    // if (!GetWidgetController())
-    // {
-    //     UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("广播初始值失败!"))
-    //    return;
-    // }
-    // GetWidgetController()->BroadcastInitialValues();
+	Super::NativeConstruct();
+	InitVerticalArea();
+	// if (!GetWidgetController())
+	// {
+	//     UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("广播初始值失败!"))
+	//    return;
+	// }
+	// GetWidgetController()->BroadcastInitialValues();
 }
-
 
 
 UBaseUserWidget* UAttributeMenuWidget::FindWidgetByTagFromArray(const TArray<UBaseUserWidget*>& Array,
                                                                 const FGameplayTag& Tag)
 {
-    for (const auto Widget : Array)
-    {
-        if (Widget->GetCurrentGameplayTag() == Tag)
-        {
-            return Widget;
-        }
-    }
-    return nullptr;
+	for (const auto Widget : Array) { if (Widget->GetCurrentGameplayTag() == Tag) { return Widget; } }
+	return nullptr;
 }
 
 void UAttributeMenuWidget::CreateVerticalAreaWidgets(const TArray<FGameplayTag*>* Array,
                                                      const EGameplayTagType GameplayTagType,
                                                      UVerticalBox* VerticalArea)
 {
-    if (!Array || !VerticalArea)
-    {
-        UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("标签数组和垂直区域内容不能为空!"));
-        return;
-    }
-    VerticalArea->ClearChildren();
-    for (const auto GameplayTag : *Array)
-    {
-        const auto Widget = CreateWidget<UBaseUserWidget>(GetOwningPlayer(), TextValueRowButtonWidgetClass);
-        Widget->SetCurrentGameplayTag(*GameplayTag);
-        VerticalArea->AddChild(Widget);
+	if (!Array || !VerticalArea)
+	{
+		UE_LOG(UAttributeMenuWidgetLog, Error, TEXT("标签数组和垂直区域内容不能为空!"));
+		return;
+	}
+	VerticalArea->ClearChildren();
+	for (const auto GameplayTag : *Array)
+	{
+		const auto Widget = CreateWidget<UBaseUserWidget>(GetOwningPlayer(), TextValueRowButtonWidgetClass);
+		Widget->SetCurrentGameplayTag(*GameplayTag);
+		VerticalArea->AddChild(Widget);
 
-        switch (GameplayTagType)
-        {
-            case EGameplayTagType::AllGameplayTags:
-                break;
-            case EGameplayTagType::VitalGameplayTags:
-                VitalAttriAreaWidgetArray.Add(Widget);
-                break;
-            case EGameplayTagType::PrimaryGameplayTags:
-                PrimaryAttriAreaWidgetArray.Add(Widget);
-                Widget->SetPadding(FMargin(0, 0, 14, 0));
-                break;
-            case EGameplayTagType::SecondaryGameplayTags:
-                SecondaryAttriAreaWidgetArray.Add(Widget);
-                break;
-        }
 
-        AllAttriAreaWidgetArray.Add(Widget);
-    }
+		switch (GameplayTagType)
+		{
+		case EGameplayTagType::AllGameplayTags:
+			break;
+		case EGameplayTagType::VitalGameplayTags:
+			VitalAttriAreaWidgetArray.Add(Widget);
+			break;
+		case EGameplayTagType::PrimaryGameplayTags:
+			PrimaryAttriAreaWidgetArray.Add(Widget);
+			Widget->SetPadding(FMargin(0, 0, 14, 0));
+			break;
+		case EGameplayTagType::SecondaryGameplayTags:
+			SecondaryAttriAreaWidgetArray.Add(Widget);
+			break;
+		}
+
+		AllAttriAreaWidgetArray.Add(Widget);
+	}
 }
