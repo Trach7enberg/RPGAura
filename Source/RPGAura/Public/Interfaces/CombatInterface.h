d4f773d023c02972bcccda9d49a5c5554aff5bab
my-laptop <2655667253@qq.com>
2024-08-11 20:58:03 +0800

    添加:UCombatInterface新增函数GetCombatSocketLocation,获取武器攻击的socket位置(比如子弹、火球发射的位置)

--- Source/RPGAura/Public/Interfaces/CombatInterface.h
+++ Source/RPGAura/Public/Interfaces/CombatInterface.h
@@ -6,11 +6,11 @@
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
-/// 
-UINTERFACE(MinimalAPI)
+/// 和战斗相关的接口
+UINTERFACE(MinimalAPI, meta=(CannotImplementInterfaceInBlueprint))
 class UCombatInterface : public UInterface
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 };
 
 /**
@@ -18,14 +18,19 @@ class UCombatInterface : public UInterface
  */
 class RPGAURA_API ICombatInterface
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    /// 获取当前角色的等级
-    /// @return 等级
-    virtual int32 GetCharacterLevel() = 0;
+	/// 获取当前角色的等级
+	/// @return 等级
+	virtual int32 GetCharacterLevel() = 0;
 
-    /// 获取武器攻击的socket位置(比如子弹、火球发射的位置)
-    /// @return 
-    virtual FVector GetCombatSocketLocation() = 0;
+	/// 获取武器攻击的socket位置(比如子弹、火球发射的位置)
+	/// @return 
+	virtual FVector GetCombatSocketLocation() = 0;
+
+	/// 使角色朝向目标位置
+	/// @param TargetLoc 目标位置
+	UFUNCTION(BlueprintCallable)
+	virtual void UpdateCharacterFacingTarget(const FVector& TargetLoc) = 0;
 };
