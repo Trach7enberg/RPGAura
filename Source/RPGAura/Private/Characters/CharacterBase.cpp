1db95edd7a6672081ed3c6c0e4487a027c4d5e3d
my-laptop <2655667253@qq.com>
2024-08-11 20:57:22 +0800

    修改:ACharacterBase初始化角色所有属性的函数现在被提前到一个函数里

--- Source/RPGAura/Private/Characters/CharacterBase.cpp
+++ Source/RPGAura/Private/Characters/CharacterBase.cpp
@@ -13,93 +13,79 @@ DEFINE_LOG_CATEGORY_STATIC(ACharacterBaseLog, All, All);
 
 ACharacterBase::ACharacterBase()
 {
-    PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 
-    WeaponLogicBaseComponent = CreateDefaultSubobject<UWeaponLogicBaseComponent>("WeaponLogicComponent");
+	WeaponLogicBaseComponent = CreateDefaultSubobject<UWeaponLogicBaseComponent>("WeaponLogicComponent");
 
-    if (GetMesh())
-    {
-        GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
-    }
+	if (GetMesh()) { GetMesh()->SetRelativeRotation(FRotator(0, -90, 0)); }
 }
 
 
 FVector ACharacterBase::GetCombatSocketLocation()
 {
-    return WeaponLogicBaseComponent->GetWeaponSocketLocByName(WeaponLogicBaseComponent->GetWeaponTipSocketName());
+	return WeaponLogicBaseComponent->GetWeaponSocketLocByName(WeaponLogicBaseComponent->GetWeaponTipSocketName());
 }
 
 void ACharacterBase::BeginPlay()
 {
-    Super::BeginPlay();
-    check(WeaponLogicBaseComponent);
+	Super::BeginPlay();
+	check(WeaponLogicBaseComponent);
 }
 
 
 void ACharacterBase::AddCharacterAbilities() const
 {
-    if (!HasAuthority())
-    {
-        return;
-    }
+	if (!HasAuthority()) { return; }
 
-    if (!GetAbilitySystemComponent())
-    {
-        return;
-    }
+	if (!GetAbilitySystemComponent()) { return; }
 
-    const auto Asc = Cast<UBaseAbilitySystemComponent>(GetAbilitySystemComponent());
-    if (!Asc)
-    {
-        return;
-    }
-    Asc->AddCharacterAbilities(StartUpAbilities);
+	const auto Asc = Cast<UBaseAbilitySystemComponent>(GetAbilitySystemComponent());
+	if (!Asc) { return; }
+	Asc->AddCharacterAbilities(StartUpAbilities);
 }
 
 void ACharacterBase::InitAttributes(const TSubclassOf<UGameplayEffect> AttributesGameplayEffect,
                                     const float Level) const
 {
-    if (!GetAbilitySystemComponent() || !AttributesGameplayEffect)
-    {
-        UE_LOG(ACharacterBaseLog, Error, TEXT("无法初始化属性，因为没有能力组件或者相应的GE为nullptr!"));
-        return;
-    }
+	if (!GetAbilitySystemComponent() || !AttributesGameplayEffect)
+	{
+		UE_LOG(ACharacterBaseLog, Error, TEXT("[%s]无法初始化属性，因为没有能力组件或者相应的GE为nullptr!"), *GetName());
+		return;
+	}
 
-    auto GEContext = GetAbilitySystemComponent()->MakeEffectContext();
-    GEContext.AddSourceObject(this);
-    const auto GESpec = GetAbilitySystemComponent()->MakeOutgoingSpec(AttributesGameplayEffect, Level, GEContext);
-    GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*GESpec.Data.Get(), GetAbilitySystemComponent());
+	auto GEContext = GetAbilitySystemComponent()->MakeEffectContext();
+	GEContext.AddSourceObject(this);
+	const auto GESpec = GetAbilitySystemComponent()->MakeOutgoingSpec(AttributesGameplayEffect, Level, GEContext);
+	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*GESpec.Data.Get(), GetAbilitySystemComponent());
+}
+
+void ACharacterBase::InitAllAttributes() const
+{
+	InitAttributes(DefaultPrimaryAttributesGameplayEffect);
+	InitAttributes(DefaultSecondaryPrimaryAttributesGameplayEffect);
+	InitAttributes(DefaultVitalAttributesGameplayEffect);
 }
 
 
 bool ACharacterBase::CanHighLight()
 {
-    const auto Can = Cast<IHighLightInterface>(this);
+	const auto Can = Cast<IHighLightInterface>(this);
 
-    return (Can) ? true : false;
+	return (Can) ? true : false;
 }
 
 void ACharacterBase::InitAbilityActorInfo() {}
 
 void ACharacterBase::HighLight()
 {
-    if (!CanHighLight())
-    {
-        return;
-    }
-    Cast<IHighLightInterface>(this)->HighLightActor();
+	if (!CanHighLight()) { return; }
+	Cast<IHighLightInterface>(this)->HighLightActor();
 }
 
 void ACharacterBase::UnHighLight()
 {
-    if (!CanHighLight())
-    {
-        return;
-    }
-    Cast<IHighLightInterface>(this)->UnHighLightActor();
+	if (!CanHighLight()) { return; }
+	Cast<IHighLightInterface>(this)->UnHighLightActor();
 }
 
-UAbilitySystemComponent* ACharacterBase::GetAbilitySystemComponent() const
-{
-    return AbilitySystemComponent;
-}
+UAbilitySystemComponent* ACharacterBase::GetAbilitySystemComponent() const { return AbilitySystemComponent; }
--- Source/RPGAura/Public/Characters/CharacterBase.h
+++ Source/RPGAura/Public/Characters/CharacterBase.h
@@ -21,69 +21,72 @@ class UWeaponLogicBaseComponent;
 UCLASS(Abstract)
 class RPGAURA_API ACharacterBase : public ACharacter, public IAbilitySystemInterface, public ICombatInterface
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    ACharacterBase();
+	ACharacterBase();
 
-    /// 高亮当前角色
-    virtual void HighLight();
+	/// 高亮当前角色
+	virtual void HighLight();
 
-    /// 解除高亮当前角色
-    virtual void UnHighLight();
+	/// 解除高亮当前角色
+	virtual void UnHighLight();
 
-    virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-    // ~ IAbilitySystemInterface
-    virtual UAttributeSet* GetAttributeSet() const
-    {
-        return AttributeSet;
-    }
+	// ~ IAbilitySystemInterface
+	virtual UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
-    // ~ ICombatInterface
-    virtual int32 GetCharacterLevel() override
-    {
-        return 0;
-    };
-    virtual FVector GetCombatSocketLocation() override;
-    // ~ ICombatInterface
+	// ~ ICombatInterface
+	virtual int32 GetCharacterLevel() override { return 0; };
+	virtual FVector GetCombatSocketLocation() override;
+
+	virtual void UpdateCharacterFacingTarget(const FVector& TargetLoc) override {};
+	// ~ ICombatInterface
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
-    UPROPERTY(EditDefaultsOnly, Category="Weapon")
-    TObjectPtr<UWeaponLogicBaseComponent> WeaponLogicBaseComponent;
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	TObjectPtr<UWeaponLogicBaseComponent> WeaponLogicBaseComponent;
 
-    /// GAS的能力组件
-    UPROPERTY()
-    TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	/// GAS的能力组件
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
-    /// GAS的属性集
-    UPROPERTY()
-    TObjectPtr<UAttributeSet> AttributeSet;
+	/// GAS的属性集
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 
-    // 用于初始化角色主要属性的GE类
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
-    TSubclassOf<UGameplayEffect> DefaultPrimaryAttributesGameplayEffect = nullptr;
+	// 用于初始化角色主要属性的GE类
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
+	TSubclassOf<UGameplayEffect> DefaultPrimaryAttributesGameplayEffect = nullptr;
 
-    // 用于初始化角色次要属性的GE类,在主属性初始化后执行,该GE为infinite
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
-    TSubclassOf<UGameplayEffect> DefaultSecondaryPrimaryAttributesGameplayEffect = nullptr;
+	// 用于初始化角色次要属性的GE类,在主属性初始化后执行,该GE为infinite
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
+	TSubclassOf<UGameplayEffect> DefaultSecondaryPrimaryAttributesGameplayEffect = nullptr;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Abilities")
-    TArray<TSubclassOf<UGameplayAbility>> StartUpAbilities;
+	// 用于初始化角色重要(Vital)属性的GE类,在SecondPrimary属性初始化后执行
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
+	TSubclassOf<UGameplayEffect> DefaultVitalAttributesGameplayEffect = nullptr;
 
-    /// 给角色授予能力(初始能力)
-    void AddCharacterAbilities() const;
-    
-    /// 接受一个GE用来初始化角色身上的属性,次要属性必须得在主要属性初始化之后
-    /// @param AttributesGameplayEffect GE类
-    /// @param Level 应用GE的等级
-    virtual void InitAttributes(TSubclassOf<UGameplayEffect> AttributesGameplayEffect, float Level = 1.f) const;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Abilities")
+	TArray<TSubclassOf<UGameplayAbility>> StartUpAbilities;
 
-    /// 当前角色能否被高亮
-    /// @return 能高亮则返回true
-    virtual bool CanHighLight();
+	/// 给角色授予能力(初始能力)
+	void AddCharacterAbilities() const;
 
-    virtual void InitAbilityActorInfo();
+	/// 接受一个GE用来初始化角色身上的属性,次要属性必须得在主要属性初始化之后
+	/// @param AttributesGameplayEffect GE类
+	/// @param Level 应用GE的等级
+	virtual void InitAttributes(TSubclassOf<UGameplayEffect> AttributesGameplayEffect, float Level = 1.f) const;
+
+	/// 初始化角色身上的所有属性
+	void InitAllAttributes() const;
+
+	/// 当前角色能否被高亮
+	/// @return 能高亮则返回true
+	virtual bool CanHighLight();
+
+	virtual void InitAbilityActorInfo();
 };
