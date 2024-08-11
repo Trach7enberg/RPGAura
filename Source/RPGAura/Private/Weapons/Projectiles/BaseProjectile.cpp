6af5a1c3ba13a632ca0f312429d949d0e1ef6b37
my-laptop <2655667253@qq.com>
2024-08-11 20:53:48 +0800

    添加:ABaseProjectile 飞弹类Overlap时候生成特效和声音,以及被生成时候就播放的滋滋声,被Overlap的对象可以被应用GameplayEffects

--- Source/RPGAura/Private/Weapons/Projectiles/BaseProjectile.cpp
+++ Source/RPGAura/Private/Weapons/Projectiles/BaseProjectile.cpp
@@ -3,42 +3,97 @@
 
 #include "Weapons/Projectiles/BaseProjectile.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 #include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "RPGAura/RPGAura.h"
 
-// Sets default values
+DEFINE_LOG_CATEGORY_STATIC(ABaseProjectileLog, All, All);
+
 ABaseProjectile::ABaseProjectile()
 {
-    PrimaryActorTick.bCanEverTick = false;
-    bReplicates = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
 
-    SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
-    ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
-    FireBoltNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NiagaraComponent");
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+	FireBoltNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NiagaraComponent");
 
-    SphereComponent->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
-    SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
-    SphereComponent->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
-    SphereComponent->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
-    SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	SphereComponent->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
-    ProjectileMovementComponent->InitialSpeed = 550.f;
-    ProjectileMovementComponent->MaxSpeed = 550.f;
-    ProjectileMovementComponent->ProjectileGravityScale = 0.f;
+	ProjectileMovementComponent->InitialSpeed = 550.f;
+	ProjectileMovementComponent->MaxSpeed = 550.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.f;
 
-    SetRootComponent(SphereComponent);
-    FireBoltNiagaraComponent->SetupAttachment(GetRootComponent());
+	SetRootComponent(SphereComponent);
+	FireBoltNiagaraComponent->SetupAttachment(GetRootComponent());
+
+	SphereComponent->SetCollisionProfileName(COLLISION_PRESET_PROJECTILE);
 }
 
-// Called when the game starts or when spawned
+
 void ABaseProjectile::BeginPlay()
 {
-    Super::BeginPlay();
-    SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABaseProjectile::OnSphereOverlap);
+	Super::BeginPlay();
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABaseProjectile::OnSphereOverlap);
+
+	SetLifeSpan(ProjectileLifeSpawn);
+	if (LoopingSound)
+	{
+		LoopSoundAudioComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+	}
 }
 
-void ABaseProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ABaseProjectile::SpawnVfxAndSound() const
 {
-    //
+	if (!ImpactSound || !ImpactEffect) { return; }
+	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+}
+
+void ABaseProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+	SpawnVfxAndSound();
+	if (LoopSoundAudioComponent) { LoopSoundAudioComponent.Get()->Stop(); }
+
+	if (HasAuthority())
+	{
+		UAbilitySystemComponent *ActorAsc = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor);
+		// TODO 应用GE
+		if (!DamageEffectSpecHandle.IsValid() || !ActorAsc)
+		{
+			UE_LOG(ABaseProjectileLog, Warning, TEXT("无法应用GE!"));
+		}else{
+			// 应用GE到自身,这里的自身是OtherActor
+			ActorAsc->ApplyGameplayEffectSpecToSelf(*DamageEffectSpecHandle.Data);
+		}
+		 
+		
+		Destroy();
+	}
+	else
+	{
+		// 投射物有可能在还没有生成特效和声音的时候就被销毁了(重叠在服务器上发生,销毁的行为会复制到客户端会导致客户端还没有重叠的时候就销毁了飞弹)
+		// 因此我们标记一下当前客户端的投射物已经击中了,然后重写Destroy函数,在被销毁前播放特效和声音
+		BIsHit = true;
+	}
+}
+
+void ABaseProjectile::Destroyed()
+{
+	if (!BIsHit && !HasAuthority()) { SpawnVfxAndSound(); }
+
+	if (LoopSoundAudioComponent) { LoopSoundAudioComponent.Get()->Stop(); }
+	Super::Destroyed();
 }
--- Source/RPGAura/Public/Weapons/Projectiles/BaseProjectile.h
+++ Source/RPGAura/Public/Weapons/Projectiles/BaseProjectile.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "GameFramework/Actor.h"
 #include "BaseProjectile.generated.h"
 
@@ -16,25 +17,56 @@ class USphereComponent;
 UCLASS()
 class RPGAURA_API ABaseProjectile : public AActor
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    ABaseProjectile();
+	ABaseProjectile();
+	
+	UPROPERTY(BlueprintReadWrite, meta=(ExposeOnSpawn=true), Category="GameplayEffect")
+	FGameplayEffectSpecHandle DamageEffectSpecHandle;
+	
+	virtual void Destroyed() override;
 
 protected:
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Collision")
-    TObjectPtr<USphereComponent> SphereComponent;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Projectile")
-    TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Projectile")
+	float ProjectileLifeSpawn = 5.0f;
 
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Projectile")
-    TObjectPtr<UNiagaraComponent> FireBoltNiagaraComponent;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Collision")
+	TObjectPtr<USphereComponent> SphereComponent;
 
-    virtual void BeginPlay() override;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Niagara")
+	TObjectPtr<UNiagaraComponent> FireBoltNiagaraComponent;
+
+	// 投射物冲击时的效果
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Niagara")
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	// 冲击声
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
+	TObjectPtr<USoundBase> ImpactSound;
+
+	// 循环声
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
+	TObjectPtr<USoundBase> LoopingSound;
+
+
+	virtual void BeginPlay() override;
 
 private:
-    UFUNCTION()
-    void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	// 当前投射物有没有Overlap
+	bool BIsHit;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                     int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/// 生成特效和声音
+	void SpawnVfxAndSound() const;
+
+	TObjectPtr<UAudioComponent> LoopSoundAudioComponent;
 };
