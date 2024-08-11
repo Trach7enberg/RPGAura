b0ddc87c7fd8f83984f3509744b1bbee8e169df1
my-laptop <2655667253@qq.com>
2024-08-11 20:42:22 +0800

    添加:在AAuraCharacter中新增运动扭曲组件和更新运动扭曲组件扭曲的方向函数

--- Source/RPGAura/Private/Characters/AuraCharacter.cpp
+++ Source/RPGAura/Private/Characters/AuraCharacter.cpp
@@ -4,6 +4,7 @@
 #include "Characters/AuraCharacter.h"
 
 #include "AbilitySystemComponent.h"
+#include "MotionWarpingComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -16,143 +17,119 @@ DEFINE_LOG_CATEGORY_STATIC(AAuraCharacterLog, All, All);
 
 AAuraCharacter::AAuraCharacter()
 {
-    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
-    CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+	MotionWarpingComponent = CreateDefaultSubobject<UMotionWarpingComponent>("MotionWarping");
 
-    SpringArmComponent->SetupAttachment(GetRootComponent());
-    CameraComponent->SetupAttachment(SpringArmComponent);
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	CameraComponent->SetupAttachment(SpringArmComponent);
 
-    SpringArmComponent->SetRelativeLocation(FVector(0, 0, 85));
-    SpringArmComponent->SetRelativeRotation(FRotator(-45, 0, 0));
-    SpringArmComponent->TargetArmLength = 750.f;
-    SpringArmComponent->bEnableCameraLag = true;
-    SpringArmComponent->bInheritPitch = false;
-    SpringArmComponent->bInheritRoll = false;
-    SpringArmComponent->bInheritYaw = false;
 
-    GetCharacterMovement()->bOrientRotationToMovement = true;
-    GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
+	SpringArmComponent->SetRelativeLocation(FVector(0, 0, 85));
+	SpringArmComponent->SetRelativeRotation(FRotator(-45, 0, 0));
+	SpringArmComponent->TargetArmLength = 750.f;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->bInheritPitch = false;
+	SpringArmComponent->bInheritRoll = false;
+	SpringArmComponent->bInheritYaw = false;
 
-    // 限制角色的移动能力在平面上 
-    GetCharacterMovement()->bConstrainToPlane = true;
-    GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
 
-    bUseControllerRotationRoll = false;
-    bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
+	// 限制角色的移动能力在平面上 
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
-    if (GetMesh())
-    {
-        GetMesh()->SetRelativeLocation(FVector(0, 0, -85));
-    }
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+
+	if (GetMesh()) { GetMesh()->SetRelativeLocation(FVector(0, 0, -85)); }
 }
 
-void AAuraCharacter::BeginPlay()
-{
-    Super::BeginPlay();
-}
+void AAuraCharacter::BeginPlay() { Super::BeginPlay(); }
 
-UAbilitySystemComponent* AAuraCharacter::GetAbilitySystemComponent() const
-{
-    return AbilitySystemComponent;
-}
+UAbilitySystemComponent* AAuraCharacter::GetAbilitySystemComponent() const { return AbilitySystemComponent; }
 
 void AAuraCharacter::InitAbilityActorInfo()
 {
-    const auto MyPlayerState = GetPlayerState<ABasePlayerState>();
-    if (!MyPlayerState)
-    {
-        UE_LOG(AAuraCharacterLog, Error, TEXT("%s:MyPlayerState 不能为null"), *GetName());
-        return;
-    }
+	const auto MyPlayerState = GetPlayerState<ABasePlayerState>();
+	if (!MyPlayerState)
+	{
+		UE_LOG(AAuraCharacterLog, Error, TEXT("%s:MyPlayerState 不能为null"), *GetName());
+		return;
+	}
 
-    AbilitySystemComponent = MyPlayerState->GetAbilitySystemComponent();
-    AttributeSet = MyPlayerState->GetAttributeSet();
+	AbilitySystemComponent = MyPlayerState->GetAbilitySystemComponent();
+	AttributeSet = MyPlayerState->GetAttributeSet();
 
-    const auto MyAsc = Cast<UBaseAbilitySystemComponent>(AbilitySystemComponent);
-    if (!MyAsc)
-    {
-        UE_LOG(AAuraCharacterLog, Error, TEXT("MyAsc 不能为null"));
-        return;
-    }
-    MyAsc->InitAbilityActorInfo(MyPlayerState, this);
-    MyAsc->InitSetting();
+	const auto MyAsc = Cast<UBaseAbilitySystemComponent>(AbilitySystemComponent);
+	if (!MyAsc)
+	{
+		UE_LOG(AAuraCharacterLog, Error, TEXT("MyAsc 不能为null"));
+		return;
+	}
+	MyAsc->InitAbilityActorInfo(MyPlayerState, this);
+	MyAsc->InitSetting();
 
-    InitAttributes(DefaultPrimaryAttributesGameplayEffect);
-    InitAttributes(DefaultSecondaryPrimaryAttributesGameplayEffect);
-    InitAttributes(DefaultVitalAttributesGameplayEffect);
+	InitAllAttributes();
 }
 
 void AAuraCharacter::InitHUD() const
 {
-    const auto Pc = Cast<APlayerController>(GetController());
-    
-    if (!Pc)
-    {
-        
-        UE_LOG(AAuraCharacterLog, Error, TEXT("[模式:%s]:[%s]的控制器无效! (IsLocalPalyerController?:%s)"), *GetNetModeStr(), *GetName(), IsLocallyControlled() ? TEXT("是") : TEXT("否"));
-        return;
-    }
-    const auto Hud = Cast<ABaseHUD>(Pc->GetHUD());
-    if (!Hud)
-    {
-        return;
-    }
-    Hud->InitHudMainWidget();
+	const auto Pc = Cast<APlayerController>(GetController());
+	if (!Pc)
+	{
+		UE_LOG(AAuraCharacterLog, Error, TEXT("[模式:%s]:[%s]的控制器无效! (IsLocalPalyerController?:%s)"), *GetNetModeStr(),
+		       *GetName(), IsLocallyControlled() ? TEXT("是") : TEXT("否"));
+		return;
+	}
+	const auto Hud = Cast<ABaseHUD>(Pc->GetHUD());
+	if (!Hud) { return; }
+	Hud->InitHudMainWidget();
 }
 
 int32 AAuraCharacter::GetCharacterLevel()
 {
-    const auto MyPlayerState = GetPlayerState<ABasePlayerState>();
-    if (!MyPlayerState)
-    {
-        UE_LOG(AAuraCharacterLog, Error, TEXT("%s:PlayerState Cant be null"), *GetName());
-        return 0;
-    }
+	const auto MyPlayerState = GetPlayerState<ABasePlayerState>();
+	if (!MyPlayerState)
+	{
+		UE_LOG(AAuraCharacterLog, Error, TEXT("%s:PlayerState Cant be null"), *GetName());
+		return 0;
+	}
 
-    return MyPlayerState->GetPlayerLevel();
+	return MyPlayerState->GetPlayerLevel();
+}
+
+void AAuraCharacter::UpdateCharacterFacingTarget(const FVector& TargetLoc)
+{
+	MotionWarpingComponent->AddOrUpdateWarpTargetFromLocation(WarpTargetName, TargetLoc);
 }
 
 void AAuraCharacter::PossessedBy(AController* NewController)
 {
-    Super::PossessedBy(NewController);
-    // 为服务器初始化能力角色信息
-    InitAbilityActorInfo();
-    
+	Super::PossessedBy(NewController);
+	// 为服务器初始化能力角色信息
+	InitAbilityActorInfo();
 
-    InitHUD();
+	InitHUD();
 
-    AddCharacterAbilities();
+	AddCharacterAbilities();
 }
 
 void AAuraCharacter::OnRep_PlayerState()
 {
-    Super::OnRep_PlayerState();
-    // 为客户端初始化能力角色信息
-    InitAbilityActorInfo();
+	Super::OnRep_PlayerState();
+	// 为客户端初始化能力角色信息
+	InitAbilityActorInfo();
 
-    InitHUD();
+	InitHUD();
 }
 
 FString AAuraCharacter::GetNetModeStr() const
 {
-    FString NetMode = "";
+	FString NetMode = "Client";
 
-    switch (GetNetMode())
-    {
-        case ENetMode::NM_Client:
-            NetMode = "Client";
-        break;
-        case ENetMode::NM_Standalone:
-            NetMode = "Standalone";
-        break;
-        case ENetMode::NM_DedicatedServer:
-            NetMode = "DedicatedServer";
-        break;
-        case ENetMode::NM_ListenServer:
-            NetMode = "ListenServer";
-        break;
-        default: ;
-    }
-    return NetMode;
+	if (HasAuthority()) { NetMode = "Server"; }
+	return NetMode ;
 }
--- Source/RPGAura/Public/Characters/AuraCharacter.h
+++ Source/RPGAura/Public/Characters/AuraCharacter.h
@@ -6,6 +6,7 @@
 #include "Characters/CharacterBase.h"
 #include "AuraCharacter.generated.h"
 
+class UMotionWarpingComponent;
 class UGameplayEffect;
 class USpringArmComponent;
 class UCameraComponent;
@@ -20,12 +21,12 @@ class RPGAURA_API AAuraCharacter : public ACharacterBase
 
 public:
 	AAuraCharacter();
-	virtual UAbilitySystemComponent *GetAbilitySystemComponent() const override;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 
 	/// 服务器调用的函数,在此之前已经设置好玩家的控制器
 	/// @param NewController 
-	virtual void PossessedBy(AController *NewController) override;
+	virtual void PossessedBy(AController* NewController) override;
 
 	/// 客户端调用的函数,当PlayerState已经被复制
 	virtual void OnRep_PlayerState() override;
@@ -35,6 +36,8 @@ public:
 
 	// ~ ICombatInterface
 	virtual int32 GetCharacterLevel() override;
+	
+	virtual void UpdateCharacterFacingTarget(const FVector& TargetLoc) override;
 	// ~ ICombatInterface
 
 protected:
@@ -42,22 +45,24 @@ protected:
 
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Camera")
-	UCameraComponent *CameraComponent;
+	UCameraComponent* CameraComponent;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Camera")
-	USpringArmComponent *SpringArmComponent;
+	USpringArmComponent* SpringArmComponent;
 
-	// 用于初始化角色重要(Vital)属性的GE类,在SecondPrimary属性初始化后执行
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="GAS | Attributes")
-	TSubclassOf<UGameplayEffect> DefaultVitalAttributesGameplayEffect = nullptr;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation")
+	UMotionWarpingComponent* MotionWarpingComponent;
+
+	/// 动画蒙太奇中的运动扭曲的WarpTargetName
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation")
+	FName WarpTargetName = "FacingTarget";
+
 	
+
 
 	/// 初始化当前ACS的能力组件、属性集和ACS的AbilityActorInfo
 	virtual void InitAbilityActorInfo() override;
 
 private :
-    
-    FString GetNetModeStr() const;
-	
-
+	FString GetNetModeStr() const;
 };
