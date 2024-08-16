e3c6c73359c485da89aef16a0d44015203222d29
my-laptop <2655667253@qq.com>
2024-08-16 16:50:27 +0800

    添加:UCharacterClassInfo添加基于角色等级的伤害计算系数表

--- Source/RPGAura/Public/GAS/Data/CharacterClassInfo.h
+++ Source/RPGAura/Public/GAS/Data/CharacterClassInfo.h
@@ -23,20 +23,24 @@ public:
 	TMap<ECharacterClass,FCharacterClassDefaultInfo> CharacterClassInformation;
 
 	// 角色(敌人)的共同属性(Secondary)影响初始化(注意,当前敌人的次要属性不会受到主属性的升级影响,即只是Instant,不是无限效果)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> SecondaryAttributesEnemy;
 
 	// 角色(玩家)的共同属性(Secondary)影响初始化(注意,当前玩家的次要属性会受到主属性影响)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> SecondaryAttributesPlayer;
 
 	// 角色的共同属性(Vital)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> VitalAttributes;
 
 	// 角色共同拥有的能力
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|Abilities")
 	TArray<TSubclassOf<UGameplayAbility>> CommonAbilities;
+
+	// 角色的伤害计算系数表
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|CurveTables")
+	TObjectPtr<UCurveTable> DamageCalculationFactors;
 	
 	/// 根据角色职业枚举查找对应角色的默认职业数据
 	/// @param CharacterClass 角色职业枚举
