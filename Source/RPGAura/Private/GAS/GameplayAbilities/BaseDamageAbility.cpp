a6d833a90c6e47d47ac3ee1d42a64212c68a6e41
my-laptop <2655667253@qq.com>
2024-08-21 22:16:43 +0800

    修改:在UBaseDamageAbility的AssignTagSetByCallerMagnitudeWithDamageType函数中,不仅分配SetByCaller 还给GE上下文传递当前能力的伤害类型以便后面的执行计算类计算抗性

--- Source/RPGAura/Private/GAS/GameplayAbilities/BaseDamageAbility.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseDamageAbility.cpp
@@ -4,22 +4,27 @@
 #include "GAS/GameplayAbilities/BaseDamageAbility.h"
 
 #include "AbilitySystemBlueprintLibrary.h"
+#include "CoreTypes/RPGAuraGasCoreTypes.h"
+#include "GAS/Globals/GameAbilitySystemGlobals.h"
 
 DEFINE_LOG_CATEGORY_STATIC(UBaseDamageAbilityLog, All, All);
 
-void UBaseDamageAbility::AssignTagSetByCallerMagnitudeWithDamageType(const FGameplayEffectSpecHandle& SpecHandle,
-                                                                     const float AbilityLevel) const
+void UBaseDamageAbility::AssignTagSetByCallerMagnitudeWithDamageType(const FGameplayEffectSpecHandle& SpecHandle, const float AbilityLevel) const
 {
-	if (!DamageTypesMap.Num())
+	const auto Context = UGameAbilitySystemGlobals::GetCustomGeContext(SpecHandle.Data.Get()->GetContext().Get());
+	if (!Context||!DamageTypesMap.Num() || !SpecHandle.IsValid()  )
 	{
-		UE_LOG(UBaseDamageAbilityLog, Warning, TEXT("DamageTypesMap为空!"));
+		UE_LOG(UBaseDamageAbilityLog, Warning, TEXT("DamageTypesMap为空或者SepcHandle无效!"));
 		return;
 	}
+
+
 	for (const auto& Pair : DamageTypesMap)
 	{
 		// 分配一个键值对给SetByCaller,键是游戏标签,值是设定的Magnitude,到时候在GE蓝图中选择我们分配的标签,该GE就会应用我们这里设定的Magnitude值
 		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle,
 		                                                              Pair.Key,
 		                                                              Pair.Value.GetValueAtLevel(AbilityLevel));
+		Context->AddDamageType(Pair.Key);
 	}
 }
--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseDamageAbility.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseDamageAbility.h
@@ -15,17 +15,17 @@ class RPGAURA_API UBaseDamageAbility : public UBaseGameplayAbility
 	GENERATED_BODY()
 
 protected:
-	
 	// 伤害GE实体类
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GameplayEffect")
 	TSubclassOf<UGameplayEffect> DamageEffectClass;
 
 	// 伤害类型,GT映射到对应的曲线表
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Damage")
-	TMap<FGameplayTag,FScalableFloat> DamageTypesMap;
+	TMap<FGameplayTag, FScalableFloat> DamageTypesMap;
 
-	/// 分配一个键值对给SetByCaller
+	/// 分配一个键值对给SetByCaller,并且将当前的伤害类型传递到GE上下中
 	/// @param SpecHandle 
 	/// @param AbilityLevel 
-	void AssignTagSetByCallerMagnitudeWithDamageType(const FGameplayEffectSpecHandle &SpecHandle,float AbilityLevel) const;
+	void AssignTagSetByCallerMagnitudeWithDamageType(const FGameplayEffectSpecHandle& SpecHandle,
+	                                                 const float AbilityLevel) const;
 };
