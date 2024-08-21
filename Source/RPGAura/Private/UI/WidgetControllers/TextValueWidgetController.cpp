c494ff44b6759239fba76295253e92367784fcd4
my-laptop <2655667253@qq.com>
2024-08-21 22:00:56 +0800

    添加:TextValueWidget有自己的控制器

---
+++ Source/RPGAura/Private/UI/WidgetControllers/TextValueWidgetController.cpp
@@ -0,0 +1,8 @@
+// Copyright GGBAO 
+
+
+#include "UI/WidgetControllers/TextValueWidgetController.h"
+
+
+DEFINE_LOG_CATEGORY_STATIC(UTextValueWidgetControllerLog, All, All);
+
---
+++ Source/RPGAura/Public/UI/WidgetControllers/TextValueWidgetController.h
@@ -0,0 +1,20 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/BaseWidgetController.h"
+#include "TextValueWidgetController.generated.h"
+
+struct FGameplayAttribute;
+class UBaseAttributeSet;
+class UAttributeInfo;
+/**
+ *  文本Widget的控制器
+ */
+UCLASS()
+class RPGAURA_API UTextValueWidgetController : public UBaseWidgetController
+{
+	GENERATED_BODY()
+
+};
