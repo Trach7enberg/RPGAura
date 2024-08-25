d9f02684ecb9357e8880b743eb977f14b1058414
my-laptop <2655667253@qq.com>
2024-08-25 22:47:22 +0800

    添加和修改:Melee能力,使该能力更加通用,Melee能力类增加获取随机攻击动画的方法

--- Source/RPGAura/Private/GAS/GameplayAbilities/EnemyAbilities/MeleeAttack.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/EnemyAbilities/MeleeAttack.cpp
@@ -3,7 +3,14 @@
 
 #include "GAS/GameplayAbilities/EnemyAbilities/MeleeAttack.h"
 
+#include "CoreTypes/RPGAuraCoreTypes.h"
 
 
 DEFINE_LOG_CATEGORY_STATIC(UMeleeAttackLog, All, All);
 
+FMontageWithTag UMeleeAttack::GetRandomAttackAnim(const TArray<FMontageWithTag> MontageWithTags)
+{
+	if (MontageWithTags.Num() == 0) { return FMontageWithTag(); }
+	if (MontageWithTags.Num() == 1) { return MontageWithTags[0]; }
+	return MontageWithTags[FMath::RandRange(0, MontageWithTags.Num() - 1)];
+}
--- Source/RPGAura/Public/GAS/GameplayAbilities/EnemyAbilities/MeleeAttack.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/EnemyAbilities/MeleeAttack.h
@@ -6,6 +6,8 @@
 #include "GAS/GameplayAbilities/EnemyAbilities/BaseEnemyAttack.h"
 #include "MeleeAttack.generated.h"
 
+struct FMontageWithTag;
+class UAnimMontage;
 /**
  * 
  */
@@ -15,5 +17,10 @@ class RPGAURA_API UMeleeAttack : public UBaseEnemyAttack
 	GENERATED_BODY()
 
 
-	
+public:
+	/// 获取随机的攻击动画
+	/// @param MontageWithTags 
+	/// @return 
+	UFUNCTION(BlueprintCallable,Category="Combat")
+	FMontageWithTag GetRandomAttackAnim(const TArray<FMontageWithTag> MontageWithTags);
 };
