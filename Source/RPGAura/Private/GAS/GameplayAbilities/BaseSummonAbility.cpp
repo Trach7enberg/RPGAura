c69ccc381631500f91bd465e885a0353ca1212f6
my-laptop <2655667253@qq.com>
2024-08-31 23:52:21 +0800

    添加:召唤能力类现在可以在指定角度分散区域内随机生成敌人

--- Source/RPGAura/Private/GAS/GameplayAbilities/BaseSummonAbility.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseSummonAbility.cpp
@@ -3,12 +3,15 @@
 
 #include "GAS/GameplayAbilities/BaseSummonAbility.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+#include "Interfaces/CombatInterface.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 UBaseSummonAbility::UBaseSummonAbility()
 {
-	SpawnNum = 5;
-
+	SpawnNum = 0;
+	CurrentSpawnSummonIndex = 0;
 	AcceptableMinRadius = 150.f;
 	AcceptableMaxRadius = 450.f;
 	SpawnSpread = 90.f;
@@ -16,7 +19,15 @@ UBaseSummonAbility::UBaseSummonAbility()
 
 TArray<FVector> UBaseSummonAbility::GetSpawnLocation()
 {
-	if (!GetAvatarActorFromActorInfo()) { return TArray<FVector>{}; }
+	if (!GetAvatarActorFromActorInfo()) { return TArray<FVector>(); }
+
+	const auto CombatInt = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+
+	if (!CombatInt) { return TArray<FVector>(); }
+
+	// 总数等于人物当前可以召唤的最大数
+	SpawnNum = CombatInt->GetMaxSummonsCount() - CombatInt->GetCurrentSummonsCount();
+	if (!SpawnNum) { return TArray<FVector>(); }
 
 	TArray<FVector> Result;
 	const auto ForwardVector = GetAvatarActorFromActorInfo()->GetActorForwardVector();
@@ -30,42 +41,77 @@ TArray<FVector> UBaseSummonAbility::GetSpawnLocation()
 
 	const auto RightOfSpread = ForwardVector.RotateAngleAxis(SpawnSpread / 2.f, FVector::UpVector);
 
-	// 绘制夹角调试线
-
-
 	for (int i = 0; i < SpawnNum; ++i)
 	{
 		const auto TmpDeltaSpread = LeftOfSpread.RotateAngleAxis(i * DeltaSpread, FVector::UpVector);
 		const FVector RandomLocOnLine = Location + TmpDeltaSpread * FMath::RandRange(
 			AcceptableMinRadius, AcceptableMaxRadius);
-		
+
 		FHitResult HitResult;
 		// 射线检测,使得召唤物始终在地面
-		GetWorld()->LineTraceSingleByChannel(HitResult,RandomLocOnLine + FVector(0,0,400),RandomLocOnLine - FVector(0,0,400),ECC_Visibility);
+		GetWorld()->LineTraceSingleByChannel(HitResult, RandomLocOnLine + FVector(0, 0, 400),
+		                                     RandomLocOnLine - FVector(0, 0, 400), ECC_Visibility);
 
-		if(!HitResult.bBlockingHit){continue;}
-		
-		DrawDebug(Location, TmpDeltaSpread, HitResult.ImpactPoint,8,10);
+		if (!HitResult.bBlockingHit) { continue; }
+
+		// DrawDebug(Location, TmpDeltaSpread, HitResult.ImpactPoint, 8, 10);
 		Result.Add(HitResult.ImpactPoint);
 	}
-	
+
 	return Result;
+}
+
+const AActor* UBaseSummonAbility::SpawnSummon(const FVector& Location, const bool bRandomChoseSummonClass)
+{
+	if (!SummonClasses.Num()) { return nullptr; }
+
+	if (!GetAvatarActorFromActorInfo()) { return nullptr; }
+
+	const auto AvatarPawn = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+	if (!AvatarPawn) { return nullptr; }
+
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.Instigator = AvatarPawn;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+
+	const int32 Index = (bRandomChoseSummonClass)
+		                    ? FMath::RandRange(0, SummonClasses.Num() - 1)
+		                    : CurrentSpawnSummonIndex++ % SummonClasses.Num();
+	const auto SpawnActor =  GetWorld()->SpawnActor<APawn>(SummonClasses[Index],
+	                              Location + FVector(
+		                              0, 0, AvatarPawn->GetCapsuleComponent()->GetScaledCapsuleHalfHeight() * 2.f),
+	                              GetAvatarActorFromActorInfo()->GetActorForwardVector().Rotation(),
+	                              SpawnParameters);
+	
+	return SpawnActor;
 }
 
 void UBaseSummonAbility::DrawDebug(const FVector& Location, const FVector& TmpDeltaSpread,
                                    const FVector& RandomLocOnLine, const float RangeSphereRadius,
-                                   const float SpawnSphereRadius, const float RangeDuration , const float SpawnDuration ) const
+                                   const float SpawnSphereRadius, const float RangeDuration,
+                                   const float SpawnDuration) const
 {
 	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location,
-	                                     Location + TmpDeltaSpread * AcceptableMaxRadius, 5, FLinearColor::Yellow, RangeDuration);
+	                                     Location + TmpDeltaSpread * AcceptableMaxRadius, 5, FLinearColor::Yellow,
+	                                     RangeDuration);
 	// 绘制最小最大半径调试球
 	UKismetSystemLibrary::DrawDebugSphere(GetAvatarActorFromActorInfo(),
 	                                      Location + TmpDeltaSpread * AcceptableMinRadius,
-	                                      RangeSphereRadius, 12, FLinearColor::Red, RangeDuration);
+	                                      RangeSphereRadius, 12, FLinearColor::Blue, RangeDuration);
 	UKismetSystemLibrary::DrawDebugSphere(GetAvatarActorFromActorInfo(),
 	                                      Location + TmpDeltaSpread * AcceptableMaxRadius,
 	                                      RangeSphereRadius, 12, FLinearColor::Red, RangeDuration);
 	// 绘制生成点的球
 	UKismetSystemLibrary::DrawDebugSphere(GetAvatarActorFromActorInfo(), RandomLocOnLine,
 	                                      SpawnSphereRadius, 12, FLinearColor::Black, SpawnDuration);
+}
+
+void UBaseSummonAbility::SpawnActorOnDestroy(AActor* DestroyedActor)
+{
+	const auto CombatInt = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if(CombatInt)
+	{
+		CombatInt->UpdateCurrentSummonsCount(-1);
+	}
 }
--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseSummonAbility.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseSummonAbility.h
@@ -17,15 +17,7 @@ class RPGAURA_API UBaseSummonAbility : public UBaseGameplayAbility
 public:
 	UBaseSummonAbility();
 
-	/// 获取召唤物生成的位置
-	/// TODO 待修改为用EQS进行获取有效位置
-	/// @return 
-	UFUNCTION(BlueprintCallable)
-	TArray<FVector> GetSpawnLocation();
-
-	// 召唤的人数
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Summon")
-	int32 SpawnNum;
+	
 
 	// 召唤物距离目标的距离
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Summon")
@@ -40,11 +32,35 @@ public:
 
 	// 要召唤的召唤物实体类
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Summon")
-	TArray<TSubclassOf<APawn>> SummonClass;
+	TArray<TSubclassOf<APawn>> SummonClasses;
+
+
+	/// 获取召唤物生成的位置
+	/// TODO 待修改为用EQS进行获取环绕目标有效位置
+	/// @return 
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocation();
+
+	/// 生成召唤物
+	/// @param Location 召唤物位置
+	/// @param bRandomChoseSummonClass 随机从召唤物实体类数组中挑选召唤物
+	/// @return 
+	UFUNCTION(BlueprintCallable)
+	const AActor* SpawnSummon(const FVector  & Location, bool bRandomChoseSummonClass = true);
 
 private:
+
+	// 召唤的人数
+	int32 SpawnNum;
+
+	// 当前生成的召唤物索引
+	int32 CurrentSpawnSummonIndex ;
+	
 	// 绘制夹角线和调试球
 	void DrawDebug(const FVector& Location, const FVector& TmpDeltaSpread, const FVector& RandomLocOnLine,
 	               float RangeSphereRadius, float SpawnSphereRadius, float RangeDuration = 1.5,
 	               float SpawnDuration = 1.5) const;
+
+	UFUNCTION()
+	void SpawnActorOnDestroy( AActor*DestroyedActor);
 };
