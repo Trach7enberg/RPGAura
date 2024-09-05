7c87fca4660c0b01dd6801d87086b60b5c972eb7
my-laptop <2655667253@qq.com>
2024-09-05 23:23:39 +0800

    添加:技能栏中的显示技能球widget的控制器

---
+++ Source/RPGAura/Private/UI/WidgetControllers/SpellGlobeWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright GGBAO 
+
+
+#include "UI/WidgetControllers/SpellGlobeWidgetController.h"
+
+
+DEFINE_LOG_CATEGORY_STATIC(USpellGlobeWidgetControllerLog, All, All);
+
+void USpellGlobeWidgetController::BindCallBack() {}
+
+void USpellGlobeWidgetController::BroadcastInitialValues()
+{
+	
+}
