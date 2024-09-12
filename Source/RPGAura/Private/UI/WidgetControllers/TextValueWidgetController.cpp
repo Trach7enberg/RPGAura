c4bc23bf4ebaa0083695c336ac6d40d530daf1f4
my-laptop <2655667253@qq.com>
2024-09-12 23:49:30 +0800

    添加:实现TextValue控制器的绑定与广播函数,其中CostPoint用于蓝图中消耗可分配的属性点数然后升级主要属性

--- Source/RPGAura/Private/UI/WidgetControllers/TextValueWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/TextValueWidgetController.cpp
@@ -3,6 +3,47 @@
 
 #include "UI/WidgetControllers/TextValueWidgetController.h"
 
+#include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
+#include "Interfaces/PlayerInterface.h"
+#include "PlayerStates/BasePlayerState.h"
+
 
 DEFINE_LOG_CATEGORY_STATIC(UTextValueWidgetControllerLog, All, All);
 
+void UTextValueWidgetController::BindCallBack()
+{
+	if (!IsWidgetControllerParamsValid()) { return; }
+
+	const auto MyPs = Cast<ABasePlayerState>(GetWidgetControllerParams().CurrentPlayerState);
+	if (!MyPs) { return; }
+	MyPs->AssignableAttributePointsChangeDelegate.AddLambda([this](int32 NewValue)
+	{
+		OnAssignableAttributePointsChange.Broadcast(NewValue);
+	});
+}
+
+void UTextValueWidgetController::BroadcastInitialValues()
+{
+	if (!IsWidgetControllerParamsValid()) { return; }
+	const auto MyPs = Cast<ABasePlayerState>(GetWidgetControllerParams().CurrentPlayerState);
+	if (MyPs) { OnAssignableAttributePointsChange.Broadcast(MyPs->GetAssignableAttributePoints()); }
+}
+
+void UTextValueWidgetController::CostPoint(const FGameplayTag& AttributeTag)
+{
+	UpgradeAttribute(AttributeTag);
+	
+}
+
+bool UTextValueWidgetController::UpgradeAttribute(const FGameplayTag& AttributeTag)
+{
+	if (!IsWidgetControllerParamsValid()) { return false; }
+
+	const auto MyAsc =
+		Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
+
+	if (!MyAsc) { return false; }
+
+	MyAsc->UpgradeAttribute(AttributeTag);
+	return true;
+}
--- Source/RPGAura/Public/UI/WidgetControllers/TextValueWidgetController.h
+++ Source/RPGAura/Public/UI/WidgetControllers/TextValueWidgetController.h
@@ -6,15 +6,35 @@
 #include "UI/WidgetControllers/BaseWidgetController.h"
 #include "TextValueWidgetController.generated.h"
 
+
+
 struct FGameplayAttribute;
 class UBaseAttributeSet;
 class UAttributeInfo;
 /**
- *  文本Widget的控制器
+ *  TextValueRow Widget的控制器
  */
 UCLASS()
 class RPGAURA_API UTextValueWidgetController : public UBaseWidgetController
 {
 	GENERATED_BODY()
 
+public:
+	/// 可分配的属性点变化时候的委托
+	UPROPERTY(BlueprintAssignable)
+	FOnIntegerChangeSignature OnAssignableAttributePointsChange;
+	
+	virtual void BindCallBack() override;
+	virtual void BroadcastInitialValues() override;
+
+	/// 消耗点数,一次只能消耗一点
+	UFUNCTION(BlueprintCallable)
+	void CostPoint(const FGameplayTag& AttributeTag);
+
+private:
+	/// 升级对应的属性
+	/// @param AttributeTag
+	/// @return 
+	UFUNCTION(BlueprintCallable)
+	bool UpgradeAttribute(const FGameplayTag& AttributeTag); 
 };
