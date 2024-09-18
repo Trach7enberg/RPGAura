dc23eaabe7f83f2afdf920df3562c24e5b20b448
my-laptop <2655667253@qq.com>
2024-09-18 09:38:00 +0800

    添加:玩家的法术管理Widget类

---
+++ Source/RPGAura/Private/UI/Widgets/EquippedSpellMenu.cpp
@@ -0,0 +1,5 @@
+// Copyright GGBAO 
+
+
+#include "UI/Widgets/EquippedSpellMenu.h"
+
---
+++ Source/RPGAura/Public/UI/Widgets/EquippedSpellMenu.h
@@ -0,0 +1,17 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/BaseUserWidget.h"
+#include "EquippedSpellMenu.generated.h"
+
+/**
+ *  玩家的法术管理Widget
+ */
+UCLASS()
+class RPGAURA_API UEquippedSpellMenu : public UBaseUserWidget
+{
+	GENERATED_BODY()
+	
+};
