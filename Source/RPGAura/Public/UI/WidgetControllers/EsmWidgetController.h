09565f5dc5423f5ef5f732949a2182aa24f1bca7
my-laptop <2655667253@qq.com>
2024-09-18 09:38:15 +0800

    添加:法术管理界面UI的widget控制器

---
+++ Source/RPGAura/Public/UI/WidgetControllers/EsmWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/BaseWidgetController.h"
+#include "EsmWidgetController.generated.h"
+
+/**
+ * 法术管理界面UI的widget控制器
+ */
+UCLASS()
+class RPGAURA_API UEsmWidgetController : public UBaseWidgetController
+{
+	GENERATED_BODY()
+	
+};
