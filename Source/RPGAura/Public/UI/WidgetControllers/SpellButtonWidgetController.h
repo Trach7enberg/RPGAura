5dc19a3a15b3fc5dcd6f037f2e946cc7d92dccd6
my-laptop <2655667253@qq.com>
2024-09-21 23:41:44 +0800

    添加:技能树按钮Widget的控制器

---
+++ Source/RPGAura/Public/UI/WidgetControllers/SpellButtonWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/BaseWidgetController.h"
+#include "SpellButtonWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGAURA_API USpellButtonWidgetController : public UBaseWidgetController
+{
+	GENERATED_BODY()
+	
+};
