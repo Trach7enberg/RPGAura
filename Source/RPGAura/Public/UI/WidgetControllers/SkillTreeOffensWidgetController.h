03c2ef26e0bd565248b9d2245598971968ebeeb0
my-laptop <2655667253@qq.com>
2024-09-21 23:41:03 +0800

    添加:主动技能树Widget的控制器

---
+++ Source/RPGAura/Public/UI/WidgetControllers/SkillTreeOffensWidgetController.h
@@ -0,0 +1,18 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/BaseWidgetController.h"
+#include "SkillTreeOffensWidgetController.generated.h"
+
+/**
+ * 主动技能树Widget控制器
+ */
+UCLASS()
+class RPGAURA_API USkillTreeOffensWidgetController : public UBaseWidgetController
+{
+	GENERATED_BODY()
+
+	
+};
