6d440e0dbd06ca416f23794bf7f8ffc41cdbeb11
my-laptop <2655667253@qq.com>
2024-09-24 14:28:32 +0800

    添加:法术菜单上的法术球按钮控制器,其中BroadCastSpellGlobeButtonSelectedChange函数用于当前法术菜单的技能按钮被按下时进行广播状态

--- Source/RPGAura/Private/UI/WidgetControllers/SpellButtonWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/SpellButtonWidgetController.cpp
@@ -3,3 +3,18 @@
 
 #include "UI/WidgetControllers/SpellButtonWidgetController.h"
 
+#include "SubSystems/RPGAuraGameInstanceSubsystem.h"
+
+DEFINE_LOG_CATEGORY_STATIC(USpellButtonWidgetControllerLog, All, All);
+
+void USpellButtonWidgetController::BindCallBack() {}
+
+void USpellButtonWidgetController::BroadcastInitialValues() {}
+
+void USpellButtonWidgetController::BroadCastSpellGlobeButtonSelectedChange(const FGameplayTag& WidgetTag,const FGameplayTag& AbilityStatusTag)
+{
+	if (!IsWidgetControllerParamsValid()) { return; }
+
+
+	GetWidgetControllerParams().GameInstanceSubsystem->OnSpellButtonSelectedChange.Broadcast(this, WidgetTag,AbilityStatusTag);
+}
--- Source/RPGAura/Public/UI/WidgetControllers/SpellButtonWidgetController.h
+++ Source/RPGAura/Public/UI/WidgetControllers/SpellButtonWidgetController.h
@@ -7,11 +7,20 @@
 #include "SpellButtonWidgetController.generated.h"
 
 /**
- * 
+ * 法术菜单上的法术球按钮控制器
  */
 UCLASS()
 class RPGAURA_API USpellButtonWidgetController : public UBaseWidgetController
 {
 	GENERATED_BODY()
+
+public:
+	/// 当前技能按钮被按下时进行广播状态
+	/// @param WidgetTag 
+	/// @param AbilityStatusTag 
+	UFUNCTION(BlueprintCallable)
+	void BroadCastSpellGlobeButtonSelectedChange(const FGameplayTag& WidgetTag,const FGameplayTag& AbilityStatusTag);
 	
+	virtual void BindCallBack() override;
+	virtual void BroadcastInitialValues() override;
 };
