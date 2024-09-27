6e45c6c4cab333a4d41eae45822ebfc40b18cc55
my-laptop <2655667253@qq.com>
2024-09-27 22:48:05 +0800

    添加:现在可以由法术菜单装备widget的控制器广播初次技能树的技能按钮显示所需要的能力信息使得技能树能正确显示

--- Source/RPGAura/Private/UI/WidgetControllers/EsmWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/EsmWidgetController.cpp
@@ -3,10 +3,19 @@
 
 #include "UI/WidgetControllers/EsmWidgetController.h"
 
+#include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
 #include "SubSystems/RPGAuraGameInstanceSubsystem.h"
 
 void UEsmWidgetController::BindCallBack()
 {
 	
 }
-void UEsmWidgetController::BroadcastInitialValues() {}
+void UEsmWidgetController::BroadcastInitialValues()
+{
+	if (!IsWidgetControllerParamsValid()) { return; }
+	const auto MyAsc = Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
+	if(!MyAsc){return;}
+
+	// 广播技能树区域(包括被动和主动)的技能按钮更新显示所需要的能力信息
+	MyAsc->BroadCastDefaultSpellButtonAbilitiesInfo();
+}
