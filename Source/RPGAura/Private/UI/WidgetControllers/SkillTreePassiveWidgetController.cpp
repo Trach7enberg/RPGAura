99413149dd3278e0b966e6d40b067fdab235e1ac
my-laptop <2655667253@qq.com>
2024-09-27 22:40:03 +0800

    添加:被动技能树的widget控制器完善BindCallBack以及BroadcastInitialValues函数

--- Source/RPGAura/Private/UI/WidgetControllers/SkillTreePassiveWidgetController.cpp
+++ Source/RPGAura/Private/UI/WidgetControllers/SkillTreePassiveWidgetController.cpp
@@ -3,3 +3,34 @@
 
 #include "UI/WidgetControllers/SkillTreePassiveWidgetController.h"
 
+#include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
+#include "GAS/Data/TagToAbilityInfoAsset.h"
+#include "SubSystems/RPGAuraGameInstanceSubsystem.h"
+
+void USkillTreePassiveWidgetController::BindCallBack()
+{
+	if (!IsWidgetControllerParamsValid()) { return; }
+
+	const auto AbilityInfos = URPGAuraGameInstanceSubsystem::GetAbilityInfoAsset(
+		GetWidgetControllerParams().CurrentPlayerController);
+	if (!AbilityInfos) { return; }
+	
+	const auto MyAsc = Cast<UBaseAbilitySystemComponent>(GetWidgetControllerParams().CurrentAbilitySystemComponent);
+
+	if (!MyAsc) { return; }
+	MyAsc->OnAbilityStatusChanged.AddLambda(
+		[this,AbilityInfos](const FGameplayTag& AbilityTag, const FGameplayTag& AbilityStatusTag,int32 AbilityLevel)
+		{
+			// 广播AbilityInfo用于(被动)技能树技能球按钮的显示状态
+			FTagToAbilityInfo AbilityInfo = AbilityInfos->FindOffensiveAbilityInfo(AbilityTag);
+			AbilityInfo.StatusTag = AbilityStatusTag;
+			if (AbilityInfo.InfoDataAbilityIsValid())
+			{
+				GetWidgetControllerParams().GameInstanceSubsystem->OnSpellButtonAbilityInfoChange.Broadcast(AbilityInfo);
+			}
+		});
+}
+void USkillTreePassiveWidgetController::BroadcastInitialValues()
+{
+	
+}
