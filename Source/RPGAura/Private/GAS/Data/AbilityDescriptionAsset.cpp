7c16c2d6188d4bca6a89cb124d730c3ae8af0bf5
my-laptop <2655667253@qq.com>
2024-10-01 23:42:15 +0800

    添加:UAbilityDescriptionAsset技能描述数据资产类新增FindDescriptionByAbilityTag函数

--- Source/RPGAura/Private/GAS/Data/AbilityDescriptionAsset.cpp
+++ Source/RPGAura/Private/GAS/Data/AbilityDescriptionAsset.cpp
@@ -7,6 +7,12 @@
 
 FAbilityDescription UAbilityDescriptionAsset::FindDescriptionByAbilityTag(const FGameplayTag& AbilityTag)
 {
-	
+	for (const auto& Description : AbilityDescriptions)
+	{
+		if (Description.AbilityTag == AbilityTag)
+		{
+			return Description;
+		}
+	}
 	return FAbilityDescription();
 }
