214a52eecc81aaf4e532eba1d582a91f73aab149
my-laptop <2655667253@qq.com>
2024-10-01 23:40:46 +0800

    添加:基础能力类新增一个技能描述结构体变量,现在每个能力都有它们自己的能力描述以及相应的操作

--- Source/RPGAura/Private/GAS/GameplayAbilities/BaseGameplayAbility.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseGameplayAbility.cpp
@@ -3,7 +3,33 @@
 
 #include "GAS/GameplayAbilities/BaseGameplayAbility.h"
 
+#include "CoreTypes/RPGAuraCoreTypes.h"
+#include "FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h"
+#include "GAS/Data/AbilityDescriptionAsset.h"
+
 UBaseGameplayAbility::UBaseGameplayAbility()
 {
 	
 }
+
+void UBaseGameplayAbility::UpdateAbilityDescription(const FGameplayTag& AbilityTag, int32 AbilityLevel)
+{
+	
+}
+
+
+FAbilityDescription UBaseGameplayAbility::GetAbilityDescription(const FGameplayTag& AbilityTag,
+                                                                const int32 AbilityLevel)
+{
+	// TODO 可以进行优化 需要时才查找数据资产,要立即响应蓝图更改就每次都查找
+	CurrentAbilityDescription = URPGAuraBlueprintFunctionLibrary::GetAbilityDescriptionAsset()->
+			FindDescriptionByAbilityTag(AbilityTag);
+
+	// TODO 可以进行优化 只有第一次和能力升级时才更新技能描述
+	UpdateAbilityDescription(AbilityTag,AbilityLevel);
+	return CurrentAbilityDescription;
+}
+
+
+
+
--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseGameplayAbility.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseGameplayAbility.h
@@ -4,10 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
+#include "CoreTypes/RPGAuraCoreTypes.h"
 #include "BaseGameplayAbility.generated.h"
 
+
 /**
- * 
+ * 基础能力类
  */
 UCLASS()
 class RPGAURA_API UBaseGameplayAbility : public UGameplayAbility
@@ -20,4 +22,24 @@ public:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="StartUp")
     FGameplayTag DefaultInputTag;
 	
+    /// 获取当前能力的详细描述
+    /// @param AbilityTag 能力标签
+    /// @param AbilityLevel
+    /// @return
+    virtual FAbilityDescription GetAbilityDescription(const FGameplayTag& AbilityTag,
+                                                      int32 AbilityLevel);
+
+protected:
+    /// 当前能力的详细描述
+    UPROPERTY()
+	FAbilityDescription CurrentAbilityDescription = FAbilityDescription();
+
+    /// 更新当前能力的详细描述
+    /// 子类要更改对应技能的描述就覆盖此函数并且设置CurrentAbilityDescription即可
+    /// @param AbilityTag 
+    /// @param AbilityLevel 
+    virtual void UpdateAbilityDescription(const FGameplayTag& AbilityTag, int32 AbilityLevel);
+
+
+    
 };
