3752b4898efa8a6c535db9ccae5590c21d67b5e8
my-laptop <2655667253@qq.com>
2024-10-01 23:43:17 +0800

    添加:全局蓝图函数库新增多个函数,主要是获取能力数据资产函数GetAbilityDescriptionAsset、以及GetAbilityDescription,从Actor身上获取给定技能标签的详细技能描述

--- Source/RPGAura/Private/FunctionLibrary/RPGAuraBlueprintFunctionLibrary.cpp
+++ Source/RPGAura/Private/FunctionLibrary/RPGAuraBlueprintFunctionLibrary.cpp
@@ -3,27 +3,31 @@
 
 #include "FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GAS/Data/AbilityDescriptionAsset.h"
 #include "CoreTypes/RPGAuraGameplayTags.h"
+#include "GAS/AbilitySystemComp/BaseAbilitySystemComponent.h"
 #include "Interfaces/CombatInterface.h"
 
-DEFINE_LOG_CATEGORY_STATIC(URPGAuraBlueprintFunctionLibraryLog,All,All);
+DEFINE_LOG_CATEGORY_STATIC(URPGAuraBlueprintFunctionLibraryLog, All, All);
+
+TObjectPtr<UAbilityDescriptionAsset> URPGAuraBlueprintFunctionLibrary::AbilityDescriptionAsset;
+
+URPGAuraBlueprintFunctionLibrary::URPGAuraBlueprintFunctionLibrary() { LoadAbilityDescriptionAsset(this); }
 
 void URPGAuraBlueprintFunctionLibrary::FindLivePlayersWithinRadius(const AActor* Causer,
                                                                    TArray<AActor*>& OutOverlappingActors,
                                                                    const TArray<AActor*>& IgnoreActors, float Radius,
-                                                                   const FVector& SphereOrigin,bool IgnoreSelf)
+                                                                   const FVector& SphereOrigin, bool IgnoreSelf)
 {
-	if(!Causer || !IsValid(Causer))
+	if (!Causer || !IsValid(Causer))
 	{
-		UE_LOG(URPGAuraBlueprintFunctionLibraryLog,Warning,TEXT("Causer无效"));
+		UE_LOG(URPGAuraBlueprintFunctionLibraryLog, Warning, TEXT("Causer无效"));
 		return;
 	}
-	
+
 	FCollisionQueryParams SphereParams;
-	if (IgnoreSelf)
-	{
-		SphereParams.AddIgnoredActor(Causer);
-	}
+	if (IgnoreSelf) { SphereParams.AddIgnoredActor(Causer); }
 	SphereParams.AddIgnoredActors(IgnoreActors);
 
 	TArray<FOverlapResult> Overlaps;
@@ -53,9 +57,43 @@ void URPGAuraBlueprintFunctionLibrary::FindLivePlayersWithinRadius(const AActor*
 
 bool URPGAuraBlueprintFunctionLibrary::IsFriendly(const AActor* Actor1, AActor* Actor2)
 {
-	if(!Actor1 || !Actor2){return false;}
-	const auto bBothPlayers = Actor1->ActorHasTag(FRPGAuraGameplayTags::Get().Player) &&  Actor2->ActorHasTag(FRPGAuraGameplayTags::Get().Player);
-	const auto bBothEnemies = Actor1->ActorHasTag(FRPGAuraGameplayTags::Get().Enemy) &&  Actor2->ActorHasTag(FRPGAuraGameplayTags::Get().Enemy);
+	if (!Actor1 || !Actor2) { return false; }
+	const auto bBothPlayers = Actor1->ActorHasTag(FRPGAuraGameplayTags::Get().Player) && Actor2->ActorHasTag(
+		FRPGAuraGameplayTags::Get().Player);
+	const auto bBothEnemies = Actor1->ActorHasTag(FRPGAuraGameplayTags::Get().Enemy) && Actor2->ActorHasTag(
+		FRPGAuraGameplayTags::Get().Enemy);
 
 	return bBothPlayers || bBothEnemies;
+}
+
+UAbilityDescriptionAsset* URPGAuraBlueprintFunctionLibrary::GetAbilityDescriptionAsset()
+{
+	return AbilityDescriptionAsset.Get();
+}
+
+UAbilityDescriptionAsset* URPGAuraBlueprintFunctionLibrary::GetAbilityDescriptionAsset(UObject* Outer)
+{
+	LoadAbilityDescriptionAsset(Outer);
+	return AbilityDescriptionAsset.Get();
+}
+
+FAbilityDescription URPGAuraBlueprintFunctionLibrary::GetAbilityDescription(
+	AActor* Actor, UPARAM(ref) const FGameplayTag& AbilityTag)
+{
+	if (!AbilityTag.IsValid() || !Actor) { return FAbilityDescription(); }
+	const auto MyAsc = Cast<UBaseAbilitySystemComponent>(
+		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Actor));
+
+	if (!MyAsc) { return FAbilityDescription(); }
+
+	return MyAsc->GetAbilityDescriptionByAbilityTag(AbilityTag);
+}
+
+
+void URPGAuraBlueprintFunctionLibrary::LoadAbilityDescriptionAsset(UObject* Outer)
+{
+	AbilityDescriptionAsset = LoadObject<UAbilityDescriptionAsset>(
+		Outer,TEXT(
+			"/Script/RPGAura.AbilityDescriptionAsset'/Game/Blueprints/GAS/Data/DataAssets/DA_AbilitiesDescriptions.DA_AbilitiesDescriptions'"));
+	if (!AbilityDescriptionAsset) { UE_LOG(URPGAuraBlueprintFunctionLibraryLog, Error, TEXT("获取技能描述数据资产失败!")); }
 }
--- Source/RPGAura/Public/FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h
+++ Source/RPGAura/Public/FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h
@@ -6,6 +6,11 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "RPGAuraBlueprintFunctionLibrary.generated.h"
 
+
+struct FAbilityDescription;
+struct FGameplayTag;
+struct FAbilityDescElement;
+class UAbilityDescriptionAsset;
 /**
  * 当前项目的函数库
  */
@@ -15,6 +20,8 @@ class RPGAURA_API URPGAuraBlueprintFunctionLibrary : public UBlueprintFunctionLi
 	GENERATED_BODY()
 
 public:
+	URPGAuraBlueprintFunctionLibrary();
+	
 	/// 在给定球体范围内查找没有死亡的敌方角色
 	/// @param Causer 发起者
 	/// @param OutOverlappingActors 返回找到的没有死亡并且发生重叠的角色
@@ -33,5 +40,24 @@ public:
 	/// @param Actor2 
 	/// @return 
 	UFUNCTION(BlueprintCallable)
-	static bool IsFriendly(const AActor* Actor1, AActor* Actor2); 
+	static bool IsFriendly(const AActor* Actor1, AActor* Actor2);
+
+	/// 获取描述技能信息的数据资产
+	static UAbilityDescriptionAsset* GetAbilityDescriptionAsset();
+	
+	static UAbilityDescriptionAsset* GetAbilityDescriptionAsset(UObject* Outer);
+
+	/// 从Actor身上获取给定技能标签的详细技能描述
+	/// @param Actor 
+	/// @param AbilityTag 
+	/// @return
+	UFUNCTION(BlueprintCallable)
+	static FAbilityDescription GetAbilityDescription(AActor *Actor, const FGameplayTag& AbilityTag);
+	
+
+protected:
+	static TObjectPtr<UAbilityDescriptionAsset> AbilityDescriptionAsset;
+
+private:
+	static void LoadAbilityDescriptionAsset(UObject* Outer);
 };
