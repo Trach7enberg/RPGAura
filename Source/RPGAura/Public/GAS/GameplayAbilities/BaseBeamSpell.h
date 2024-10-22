4949fc516502b59f1f787e20bdc31738ce7d7a3a
my-laptop <2655667253@qq.com>
2024-10-22 08:48:59 +0800

    添加:新增基础光线能力类

---
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseBeamSpell.h
@@ -0,0 +1,17 @@
+// Copyright GGBAO 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/GameplayAbilities/BaseDamageAbility.h"
+#include "BaseBeamSpell.generated.h"
+
+/**
+ * 基础射线法术类
+ */
+UCLASS()
+class RPGAURA_API UBaseBeamSpell : public UBaseDamageAbility
+{
+	GENERATED_BODY() 
+	
+};
