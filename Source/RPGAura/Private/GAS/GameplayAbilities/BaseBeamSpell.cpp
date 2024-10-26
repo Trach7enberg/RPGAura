d916594cee21412d6ced4c0ab5a6f03c17fc7578
my-laptop <2655667253@qq.com>
2024-10-26 23:43:31 +0800

    添加:新的能力类 射线能力(例如雷电射线),这种射线可以持续释放、可以选择穿透敌人、分叉、以及从被击中的第一个敌人开始每次寻找最近的敌人进行传播闪电链

--- Source/RPGAura/Private/GAS/GameplayAbilities/BaseBeamSpell.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseBeamSpell.cpp
@@ -3,3 +3,282 @@
 
 #include "GAS/GameplayAbilities/BaseBeamSpell.h"
 
+#include "GameplayCueFunctionLibrary.h"
+#include "CoreTypes/RPGAuraGameplayTags.h"
+#include "FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h"
+#include "Interfaces/CombatInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+DEFINE_LOG_CATEGORY_STATIC(UBaseBeamSpellLog, All, All);
+
+void UBaseBeamSpell::TracingTarget(const FVector TracingStartPoint,
+                                   TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+                                   const ETraceTypeQuery TraceChannel, const bool bDebug)
+{
+	if (!GetAvatarActorFromActorInfo()) { return; }
+
+	const auto ComInt = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if (!ComInt) { return; }
+
+	// 击中数组的结果顺序是按照射线击中的顺序来排列的
+	const auto BlockingHit = UKismetSystemLibrary::SphereTraceMulti(this, TracingStartPoint, CursorImpactPoint, 10.f,
+	                                                                TraceChannel, false,
+	                                                                ActorsToIgnore,
+	                                                                bDebug
+		                                                                ? EDrawDebugTrace::ForDuration
+		                                                                : EDrawDebugTrace::None,
+	                                                                HitsResults, true);
+
+	if (BlockingHit && HitsResults.Num())
+	{
+		// 获取第一个击中的Actor
+		CursorImpactPoint = HitsResults.Last().ImpactPoint;
+		FirstTraceDetectedActor = HitsResults[0].GetActor();
+		BeamEndLoc = CursorImpactPoint;
+
+		// 从第一个Actor开始,按照给定半径检测附近实现了combat接口并且存活的角色
+		OverlapActors.Reset();
+		URPGAuraBlueprintFunctionLibrary::FindLivePlayersWithinRadius(GetAvatarActorFromActorInfo(), OverlapActors,
+		                                                              TArray<AActor*>{}, 650,
+		                                                              BeamEndLoc, true,
+		                                                              FRPGAuraGameplayTags::Get().Player,
+		                                                              ChainReactionNum);
+
+		if (bDebug)
+		{
+			UKismetSystemLibrary::DrawDebugSphere(this, FirstTraceDetectedActor->GetActorLocation(), 650.f, 16,
+			                                      FLinearColor::White, 6.f);
+			GEngine->AddOnScreenDebugMessage(1, 10, FColor::Green,
+			                                 FString::Printf(
+				                                 TEXT("[Limited -> %d]OverlapActors: %d"), ChainReactionNum,
+				                                 OverlapActors.Num()));
+		}
+		if (!CanApplyGE() || !OverlapActors.Num()) { return; }
+		const auto CombInterF = Cast<ICombatInterface>(FirstTraceDetectedActor);
+		if (!CombInterF->GetPreOnDeathDelegate().IsAlreadyBound(this, &UBaseBeamSpell::StartTargetPreOnDeath))
+		{
+			CombInterF->GetPreOnDeathDelegate().AddDynamic(this, &UBaseBeamSpell::StartTargetPreOnDeath);
+		}
+
+		// 按照离当前玩家最近的位置开始进行排序 (这样MST最终的射线结果就会是从距离最短到最大依次有序)
+		OverlapActors.Sort([this](const AActor& A, const AActor& B)
+		{
+			return (A.GetActorLocation() - BeamEndLoc).Length() < (B.GetActorLocation() - BeamEndLoc).Length();
+		});
+
+		// 从射线检测到的第一个角色开始,依次离当前角色最近的边集合
+		// 注意:CloseEdges的Index代表OverlapActors,而对应的Index中的结构代表边,并且CloseEdges[StartPos]出发结点,权值无穷大
+		// 例如:CloseEdges[0] = FCloseEdge{1,457},意思是从OverlapActors[0]到OverlapActors[1]的最近的边
+		TArray<FCloseEdge> CloseEdges{};
+		const auto StartPos = 1;
+		FindBeamChain(CloseEdges, StartPos, bDebug);
+
+		for (int i = 0; i < CloseEdges.Num(); ++i)
+		{
+			if (i == (StartPos - 1)) { continue; }
+			ActorToGcParam.Add(OverlapActors[i],
+			                   MakeBeamGameplayCueParameters(OverlapActors[i],
+			                                                 OverlapActors[i]->GetRootComponent(),
+			                                                 OverlapActors[CloseEdges[i].Adjvex]->
+			                                                 GetActorLocation()));
+		}
+	}
+}
+
+FGameplayCueParameters UBaseBeamSpell::MakeBeamGameplayCueParameters(AActor* Source,
+                                                                     USceneComponent* InTargetComp, const FVector& Loc)
+{
+	FGameplayCueParameters Parameters{};
+	Parameters.SourceObject = Source;
+	Parameters.TargetAttachComponent = InTargetComp;
+	Parameters.Location = Loc;
+
+	return Parameters;
+}
+
+AActor* UBaseBeamSpell::GetStartCueTarget()
+{
+	// 如果第一个被击中的Actor实现了战斗接口 ,则CueTarget是这个Actor
+	if (CanApplyGE()) { CueTargetActor = FirstTraceDetectedActor; }
+	else
+	// 否则是我们的化身
+	{
+		CueTargetActor = GetAvatarActorFromActorInfo();
+	}
+	return CueTargetActor.Get();
+}
+
+
+void UBaseBeamSpell::FindBeamChain(TArray<FCloseEdge>& CloseEdges,
+                                   const int StartPos, const bool bDebug) const
+{
+	if (OverlapActors.Num() < 2) { return; }
+	if (StartPos == 0) { return; }
+	// 射线链的起点,即从OverlapActors中的哪一个开始进行链式反应
+	const int Start = StartPos - 1;
+
+	// 顶点数
+	const int32 Vertex = GetValidChainNum();
+
+	constexpr int32 Max = TNumericLimits<int32>::Max();
+
+	// 矩阵图
+	TArray<TArray<int32>> Graph{};
+	// 标记已加入MST的数组
+	TArray<bool> AlreadyAddToMst{};
+	CloseEdges.Init(FCloseEdge{}, Vertex);
+	AlreadyAddToMst.Init(false, Vertex);
+
+	// 初始化图(顶点矩阵) 
+	for (int i = 0; i < Vertex; ++i)
+	{
+		if (bDebug) { UE_LOG(UBaseBeamSpellLog, Error, TEXT("[结点%-2d:%23s]"), i+1, *OverlapActors[i]->GetName()); }
+		TArray<int32> Elem{};
+		for (int j = 0; j < Vertex; ++j)
+		{
+			// if (i == j) { continue; }
+			auto Distance = (OverlapActors[j]->GetActorLocation() - OverlapActors[i]->
+				GetActorLocation()).Length();
+			Distance = (FMath::IsNearlyZero(Distance)) ? Max : Distance;
+			const auto f = FString::Printf(
+				TEXT("结点:%-2d -> |结点%2d : [%-23s]| -> 权重%-6.1f"), i + 1, j + 1, *OverlapActors[j]->GetName(),
+				Distance);
+			if (bDebug) { UE_LOG(UBaseBeamSpellLog, Warning, TEXT("%s"), *f); }
+			Elem.Add(Distance);
+		}
+		Graph.Add(Elem);
+	}
+
+	// 初始化最近边数组
+	for (int i = 0; i < Vertex; ++i)
+	{
+		CloseEdges[i].Adjvex = 0;
+		// 设置从Start点开始,依次到其它点的初始权值
+		CloseEdges[i].LowCost = Graph[i][Start];
+	}
+	// 标记起点已经加入MST
+	AlreadyAddToMst[Start] = true;
+
+	// Prim算法实现MST
+	for (int i = 0; i < Vertex - 1; ++i)
+	{
+		int32 MinDistance = Max;
+		int LowCostIndex = -1;
+		for (int j = 0; j < Vertex; ++j)
+		{
+			if (AlreadyAddToMst[j]) { continue; }
+
+			if (CloseEdges[j].LowCost < MinDistance)
+			{
+				MinDistance = CloseEdges[j].LowCost;
+				LowCostIndex = j;
+			}
+		}
+		if (LowCostIndex == -1) { break; }
+
+		// 加入MST
+		AlreadyAddToMst[LowCostIndex] = true;
+
+		// 因为新加入了新边,所以得更新其它点到这个边的最近的位置
+		for (int k = 0; k < Vertex; ++k)
+		{
+			if (AlreadyAddToMst[k]) { continue; }
+			if (Graph[LowCostIndex][k] < CloseEdges[k].LowCost)
+			{
+				CloseEdges[k].Adjvex = LowCostIndex;
+				CloseEdges[k].LowCost = Graph[LowCostIndex][k];
+			}
+		}
+	}
+
+	if (bDebug)
+	{
+		// 绘制Debug箭头
+		for (int i = 0; i < Vertex; ++i)
+		{
+			if (i == Start) { continue; }
+			const auto LineStart = OverlapActors[i]->GetActorLocation();
+			auto LineEnd = OverlapActors[CloseEdges[i].Adjvex]->GetActorLocation();
+			LineEnd.RotateAngleAxis(FMath::RandRange(-80, 80), FVector::RightVector);
+			UE_LOG(LogTemp, Warning, TEXT("%f"), CloseEdges[i].LowCost);
+			UKismetSystemLibrary::DrawDebugArrow(this, LineStart, LineEnd
+			                                     , 1200,
+			                                     FLinearColor::Green, 5, 4);
+			UKismetSystemLibrary::DrawDebugString(
+				this, (LineEnd - LineStart).GetSafeNormal() * (CloseEdges[i].LowCost / 3.f),
+				FString::Printf(TEXT("[%d]%.1f"), i, CloseEdges[i].LowCost),
+				OverlapActors[i], FLinearColor::Green, 5);
+		}
+	}
+}
+
+bool UBaseBeamSpell::CanEnableBeamChainReaction()
+{
+	if (!CanApplyGE() || !ActorToGamePlayCueParam.Num()) { return false; }
+	return true;
+}
+
+int32 UBaseBeamSpell::GetValidChainNum() const { return FMath::Min(OverlapActors.Num(), ChainReactionNum); }
+
+void UBaseBeamSpell::StartBeamChainReactionCue(TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+                                               const FGameplayTag GameplayCueTag)
+{
+	if (!CanEnableBeamChainReaction()) { return; }
+	for (auto& Pair : ActorToGcParam)
+	{
+		const auto ComInt = Cast<ICombatInterface>(Pair.Key);
+		if (!ComInt) { continue; }
+		UGameplayCueFunctionLibrary::AddGameplayCueOnActor(Pair.Key, GameplayCueTag, Pair.Value);
+
+		// TODO 绑定目标死亡回调 
+		if (ComInt->GetPreOnDeathDelegate().IsAlreadyBound(this, &UBaseBeamSpell::ChainTargetPreOnDeath)) { continue; }
+
+		ComInt->GetPreOnDeathDelegate().AddDynamic(this, &UBaseBeamSpell::ChainTargetPreOnDeath);
+	}
+}
+
+void UBaseBeamSpell::RemoveBeamChainReactionCue(TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+                                                const FGameplayTag GameplayCueTag)
+{
+	for (auto& Pair : ActorToGcParam)
+	{
+		UGameplayCueFunctionLibrary::RemoveGameplayCueOnActor(Pair.Key, GameplayCueTag, Pair.Value);
+	}
+
+	ActorToGcParam.Reset();
+}
+
+void UBaseBeamSpell::RemoveAllBeamCue(const FGameplayTag StartCueTag, const FGameplayTag GameplayCueTag,
+                                      const bool bRemoveStartCue)
+{
+	if (bRemoveStartCue)
+	{
+		UGameplayCueFunctionLibrary::RemoveGameplayCueOnActor(GetStartCueTarget(), StartCueTag,
+		                                                      StartBeamGameplayCueParam);
+	}
+	RemoveBeamChainReactionCue(ActorToGamePlayCueParam, GameplayCueTag);
+	bIsDeBuffSet = false;
+}
+
+bool UBaseBeamSpell::CanApplyGE() const
+{
+	// TODO 有很多冗余调用,是否Temp变量? 
+	if (Cast<ICombatInterface>(FirstTraceDetectedActor)) { return true; }
+
+	return false;
+}
+
+void UBaseBeamSpell::ApplyBeamGE()
+{
+	if (!CanApplyGE() || !OverlapActors.Num()) { return; }
+	const auto LocalTmpDeBuffChance = DeBuffChance;
+	DeBuffChance = (bIsDeBuffSet) ? 0.f : DeBuffChance;
+
+	const auto LocalNum = GetValidChainNum();
+	for (int i = 0; i < LocalNum; ++i) { CauseDamage(OverlapActors[i]); }
+
+	// 恢复原值 (因为CauseDamage时候会传递DeBuffChance)
+	// TODO CauseDamage 是否需要新增 参数进行控制,而不是用一个LocalTmpDeBuffChance 
+	DeBuffChance = LocalTmpDeBuffChance;
+	bIsDeBuffSet = true;
+}
--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseBeamSpell.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseBeamSpell.h
@@ -6,12 +6,186 @@
 #include "GAS/GameplayAbilities/BaseDamageAbility.h"
 #include "BaseBeamSpell.generated.h"
 
+USTRUCT()
+struct FCloseEdge
+{
+	GENERATED_BODY()
+
+	FCloseEdge() {}
+
+	/// 在overlapping中的Actor下标 
+	UPROPERTY()
+	int32 Adjvex = 0;
+
+	/// 权值
+	UPROPERTY()
+	float LowCost = 0;
+};
+
 /**
- * 基础射线法术类
+ * 基础射线法术类 
  */
 UCLASS()
 class RPGAURA_API UBaseBeamSpell : public UBaseDamageAbility
 {
-	GENERATED_BODY() 
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE FVector GetCursorImpactPoint() const { return CursorImpactPoint; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetCursorImpactPoint(const FVector& NewCursorImpactPoint)
+	{
+		this->CursorImpactPoint = NewCursorImpactPoint;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetUnderCursorHitActor(AActor* NewUnderCursorHitActor)
+	{
+		this->UnderCursorHitActor = NewUnderCursorHitActor;
+	}
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE FVector GetBeamEndLoc() { return this->BeamEndLoc; }
+
+	/// 获取第一次启动射线所需要的CueTarget
+	/// @return 
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	AActor* GetStartCueTarget();
+
+	/// 
+	/// @param Source
+	/// @param InTargetComp
+	/// @param Loc
+	/// @return 
+	UFUNCTION(BlueprintCallable)
+	FGameplayCueParameters MakeBeamGameplayCueParameters(AActor* Source,
+	                                                     USceneComponent* InTargetComp,
+	                                                     UPARAM(REF)const FVector& Loc);
+
+	/// 多重射线检测 TODO 技能升级后才允许穿透,默认等级不带穿透效果
+	/// @param TracingStartPoint
+	/// @param ActorToGcParam 
+	/// @param TraceChannel
+	/// @param bDebug 
+	UFUNCTION(BlueprintCallable)
+	void TracingTarget(const FVector TracingStartPoint,
+	                   UPARAM(REF) TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+	                   ETraceTypeQuery TraceChannel, bool bDebug = true);
+
+	/// 启动射线的链式反应
+	/// @param ActorToGcParam 
+	/// @param GameplayCueTag 
+	UFUNCTION(BlueprintCallable)
+	void StartBeamChainReactionCue(UPARAM(REF)TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+	                               const FGameplayTag GameplayCueTag);
+
+	/// 移除射线的链式反应
+	/// @param ActorToGcParam 
+	/// @param GameplayCueTag
+	UFUNCTION(BlueprintCallable)
+	void RemoveBeamChainReactionCue(UPARAM(REF)TMap<AActor*, FGameplayCueParameters>& ActorToGcParam,
+	                             const FGameplayTag GameplayCueTag);
+
+	/// 对射线目标应用GE
+	UFUNCTION(BlueprintCallable)
+	void ApplyBeamGE();
+
+	/// 移除所有Cue
+	/// @param StartCueTag
+	/// @param GameplayCueTag
+	/// @param bRemoveStartCue 
+	UFUNCTION(BlueprintCallable)
+	void RemoveAllBeamCue(FGameplayTag StartCueTag, FGameplayTag GameplayCueTag, bool bRemoveStartCue);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|Other")
+	FTimerHandle TimerHandle{};
+
+	/// 当前射线是否是穿透
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Beam")
+	bool BIsPenetration = false;
+
+	/// 是否启用射线穿透
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Beam")
+	bool BIsChainReaction = true;
+
+	// 射线链数
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Beam", meta=(EditCondition="BIsChainReaction"))
+	int ChainReactionNum = 5;
+
+	/// 鼠标检测击中的目标
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|HitActor")
+	TObjectPtr<AActor> UnderCursorHitActor = nullptr;
+
+	/// 释放射线时的射线起点
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|Loc")
+	FVector WeaponTipLoc = FVector::ZeroVector;
+
+	/// 当前射线的终点
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|Loc")
+	FVector BeamEndLoc = FVector::ZeroVector;
+
+	// 每一个(射线链中的)Actor对应的GameplayCueParam
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|GCN")
+	TMap<AActor*, FGameplayCueParameters> ActorToGamePlayCueParam{};
+
+	/// 第一次启动射线的检测结果(不是射线链的HitResult)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Prop|HitResult")
+	TArray<FHitResult> HitsResults{};
+
+	/// 第一次启动射线Cue所需要的Cue参数
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite, Category="Prop|GCN")
+	FGameplayCueParameters StartBeamGameplayCueParam{};
+
+	/// 射线目标击中死亡的回调函数
+	/// @param ChainTarget
+	UFUNCTION(BlueprintImplementableEvent)
+	void ChainTargetPreOnDeath(AActor* ChainTarget);
+
+	/// 触发射线链的第一个角色死亡时的回调函数
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartTargetPreOnDeath(AActor* StartTarget);
+
+private:
+	/// 每次启动射线都只会有几率触发一次DeBuff效果,而不是射线期间持续判断触发
+	UPROPERTY()
+	bool bIsDeBuffSet = false;
 	
+	/// 激活链式反应前所检测到Actors
+	UPROPERTY()
+	TArray<AActor*> OverlapActors{};
+
+	/// 第一次启动射线(GCN)所需要的CueTarget,不是启动射线链GCN的CueTarget
+	UPROPERTY()
+	TObjectPtr<AActor> CueTargetActor = nullptr;
+
+	/// 第一次启动射线GCN,由光线检测击中到的(第一个)目标
+	UPROPERTY()
+	TObjectPtr<AActor> FirstTraceDetectedActor = nullptr;
+
+
+	/// 鼠标击中的冲击点向量(启动射线技能时的点击)
+	UPROPERTY()
+	FVector CursorImpactPoint = FVector::ZeroVector;
+
+	UPROPERTY()
+	TArray<AActor*> ActorsToIgnore;
+
+	UFUNCTION(BlueprintCallable)
+	bool CanApplyGE() const;
+
+	/// 能否启用射线链式反应
+	/// @return 
+	bool CanEnableBeamChainReaction();
+
+	int32 GetValidChainNum() const;
+
+	/// 使用Prim算法计算最小生成树,查找由射线造成的射线链
+	/// @param CloseEdges 找到的射线链
+	/// @param StartPos 射线链的开始位置,即从OverlappingActors中的某个Actor开始
+	/// @param bDebug
+	void FindBeamChain(TArray<FCloseEdge>& CloseEdges, int StartPos,
+	                   bool bDebug = false) const;
 };
