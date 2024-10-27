e8220713818a10c41578b7f3d363020b7a35d9e4
my-laptop <2655667253@qq.com>
2024-10-27 14:33:07 +0800

    添加:项目头文件宏设置,用于指定各种lineTrace时的通道

--- Source/RPGAura/RPGAura.h
+++ Source/RPGAura/RPGAura.h
@@ -5,5 +5,8 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
-#define COLLISION_PRESET_PROJECTILE FName("Projectile")	// 碰撞预设
-#define ECC_PROJECTILE ECollisionChannel::ECC_GameTraceChannel1 // 默认情况下第一个通道是我们的自定义通道按顺序排列
+#define COLLISION_PRESET_PROJECTILE FName("Projectile")					// 碰撞预设
+#define ECC_PROJECTILE ECollisionChannel::ECC_GameTraceChannel1			// 默认情况下第一个通道是我们的自定义通道按顺序排列
+#define TRACE_TYPE_QUERY_BEAM ETraceTypeQuery::TraceTypeQuery3			// 摄像机的射线检测的追踪通道
+#define TRACE_TYPE_QUERY_CAMERA ETraceTypeQuery::TraceTypeQuery2		// 用于技能Beam射线检测的追踪通道
+#define TRACE_TYPE_QUERY_VISIBILITY ETraceTypeQuery::TraceTypeQuery1	// 可见性的射线检测的追踪通道
