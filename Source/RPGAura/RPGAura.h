3de67e4ca42c1b265db008d85e593280e02b1d62
my-laptop <2655667253@qq.com>
2024-11-01 20:32:52 +0800

    添加:项目的RPGAura头文件新增两个宏,COLLISION_PRESET_DETECT_GROUND_POINTS用于表示名为“DetectGroundPoints”的碰撞预设,TRACE_CHANNEL_MAGICDECAL用于表示自定义通道3

--- Source/RPGAura/RPGAura.h
+++ Source/RPGAura/RPGAura.h
@@ -5,8 +5,7 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
-#define COLLISION_PRESET_PROJECTILE FName("Projectile")					// 碰撞预设
-#define ECC_PROJECTILE ECollisionChannel::ECC_GameTraceChannel1			// 默认情况下第一个通道是我们的自定义通道按顺序排列
-#define TRACE_TYPE_QUERY_BEAM ETraceTypeQuery::TraceTypeQuery3			// 摄像机的射线检测的追踪通道
-#define TRACE_TYPE_QUERY_CAMERA ETraceTypeQuery::TraceTypeQuery2		// 用于技能Beam射线检测的追踪通道
-#define TRACE_TYPE_QUERY_VISIBILITY ETraceTypeQuery::TraceTypeQuery1	// 可见性的射线检测的追踪通道
+#define COLLISION_PRESET_PROJECTILE FName("Projectile")						// 碰撞预设 射弹
+#define COLLISION_PRESET_DETECT_GROUND_POINTS FName("DetectGroundPoints")	// 碰撞预设 用于检测地面上的点
+
+#define TRACE_CHANNEL_MAGICDECAL ECollisionChannel::ECC_GameTraceChannel3
