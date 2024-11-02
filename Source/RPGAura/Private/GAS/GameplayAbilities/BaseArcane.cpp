0773bd6cc6a623517e19f40f36395211cc44f1df
my-laptop <2655667253@qq.com>
2024-11-02 23:49:44 +0800

    添加:奥术能力类

--- Source/RPGAura/Private/GAS/GameplayAbilities/BaseArcane.cpp
+++ Source/RPGAura/Private/GAS/GameplayAbilities/BaseArcane.cpp
@@ -5,50 +5,78 @@
 
 #include "FunctionLibrary/RPGAuraBlueprintFunctionLibrary.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "RPGAura/RPGAura.h"
 
-void UBaseArcane::DetectGroundPoints(const FVector& StartCenter, const float Radius, const float Spread,
-                                     const int32 Points)
+DEFINE_LOG_CATEGORY_STATIC(UBaseArcaneLog, All, All);
+
+void UBaseArcane::DetectGroundPoints(const FVector& StartCenter)
 {
+	const auto LocalArcaneSpawnNum = GetValidAbilityCount(GetAbilityLevel());
 	auto LocalTmpCenter = StartCenter;
 	LocalTmpCenter.Z += 40;
 	TArray<FVector> LocalTmp{};
-	const auto NewLocalSpread = (Spread == 360.f && Points == 2) ? FMath::RandRange(45, 300) : Spread;
-	URPGAuraBlueprintFunctionLibrary::GetVectorBySpread(NewLocalSpread, Points, LocalTmpCenter.ForwardVector, LocalTmp);
+	const auto NewLocalSpread = (ArcaneSpread == 360.f && LocalArcaneSpawnNum == 2)
+		                            ? FMath::RandRange(45, 300)
+		                            : ArcaneSpread;
+	URPGAuraBlueprintFunctionLibrary::GetVectorBySpread(NewLocalSpread, LocalArcaneSpawnNum, LocalTmpCenter.ForwardVector,
+	                                                    LocalTmp);
 	GroundPoints.Reset();
-	const auto VectorLen = FMath::Max(Radius, 250.f);
 
+	const auto VectorMaxLen = FMath::Max(ArcaneOutRadius, 250.f);
+	// 生成的点位最小值根据内半径来变化
+	const auto VectorMinLen = (ArcaneInnerRadius > 0) ? ArcaneInnerRadius / 2.f : VectorMaxLen;
 	for (auto& Vector : LocalTmp)
 	{
-		const auto RandLen = FMath::RandRange(100.f, VectorLen);
-		UKismetSystemLibrary::DrawDebugArrow(this, LocalTmpCenter, LocalTmpCenter + Vector * VectorLen, 1000.f,
-		                                     FLinearColor::Red,
-		                                     5, 5);
+		const auto RandLen = FMath::RandRange(VectorMinLen, VectorMaxLen);
+
 		auto GroundPoint = LocalTmpCenter + Vector * RandLen;
-		UKismetSystemLibrary::DrawDebugSphere(this, GroundPoint, 10, 12, FLinearColor::Green, 5,
-		                                      5);
-		FHitResult HitResult;
-		UKismetSystemLibrary::LineTraceSingleByProfile(this, {GroundPoint.X, GroundPoint.Y, GroundPoint.Z + VectorLen},
-		                                               {GroundPoint.X, GroundPoint.Y, GroundPoint.Z - VectorLen},
-		                                               "DetectGroundPoints",
-		                                               true, {}, EDrawDebugTrace::Type::ForDuration, HitResult, true,
-		                                               FLinearColor::Blue, FLinearColor::Blue, 5);
+		
+		if (BShowDebugDraw)
+		{
+			UKismetSystemLibrary::DrawDebugArrow(this, LocalTmpCenter, LocalTmpCenter + Vector * VectorMaxLen, 1000.f,
+			                                     FLinearColor::Red,
+			                                     5, 5);
+			UKismetSystemLibrary::DrawDebugSphere(this, GroundPoint, 10, 12, FLinearColor::Green, 5,
+			                                      5);
+		}
+		FHitResult AdjustHitResultForVertical;
+		UKismetSystemLibrary::LineTraceSingleByProfile(
+			this, {GroundPoint.X, GroundPoint.Y, GroundPoint.Z + VectorMaxLen},
+			{GroundPoint.X, GroundPoint.Y, GroundPoint.Z - VectorMaxLen},
+			COLLISION_PRESET_DETECT_GROUND_POINTS,
+			true, {}, (BShowDebugDraw)
+				          ? EDrawDebugTrace::Type::ForDuration
+				          : EDrawDebugTrace::Type::None,
+			AdjustHitResultForVertical, true,
+			FLinearColor::Blue, FLinearColor::Blue, 5);
 		// TODO 待对水平向量进行光线检测修正,因为点可能会超出范围导致穿模 
-		GroundPoints.Add(HitResult.ImpactPoint);
+		GroundPoints.Add(AdjustHitResultForVertical.ImpactPoint);
 	}
 }
 
 void UBaseArcane::SpawnArcaneShards(const FVector& SpawnCenter)
 {
-	if (!GetWorld()) { return; }
+	if (!GetWorld())
+	{
+		K2_EndAbility();
+		return;
+	}
 	if (GroundPoints.Num() && GetWorld()->GetTimerManager().IsTimerPaused(ArcaneTimeHandle))
 	{
 		LocalPointsIndex = 0;
 		// 首先在中心生成一个奥术碎片
-		FGameplayCueParameters Parameters{};
-		Parameters.Location = SpawnCenter;
-		K2_ExecuteGameplayCueWithParams(FRPGAuraGameplayTags::Get().GameplayCue_Spawn_ArcaneShards, Parameters);
-		// 再执行多重生成碎片
+		CauseArcaneRadiusDamage(SpawnCenter);
+		if (BShowDebugDraw)
+		{
+			UKismetSystemLibrary::DrawDebugSphere(this, SpawnCenter, ArcaneOutRadius, 20, FLinearColor::Green,
+			                                      1,
+			                                      1);
+			UKismetSystemLibrary::DrawDebugSphere(this, SpawnCenter, ArcaneInnerRadius, 20, FLinearColor::Green,
+			                                      1,
+			                                      1);
+		}
 		URPGAuraBlueprintFunctionLibrary::ShuffleArray(GroundPoints);
+		// 再执行多重生成碎片
 		GetWorld()->GetTimerManager().UnPauseTimer(ArcaneTimeHandle);
 	}
 }
@@ -62,7 +90,8 @@ void UBaseArcane::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const
 	// 初始化定时器
 	if (GetWorld() && !GetWorld()->GetTimerManager().IsTimerActive(ArcaneTimeHandle))
 	{
-		GetWorld()->GetTimerManager().SetTimer(ArcaneTimeHandle, this, &UBaseArcane::SpawnArcaneWithDelay, 0.2f, true);
+		GetWorld()->GetTimerManager().SetTimer(ArcaneTimeHandle, this, &UBaseArcane::SpawnArcaneWithDelay,
+		                                       ArcaneSpawnDelay, true);
 		GetWorld()->GetTimerManager().PauseTimer(ArcaneTimeHandle);
 	}
 }
@@ -82,15 +111,83 @@ void UBaseArcane::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGam
 void UBaseArcane::SpawnArcaneWithDelay()
 {
 	if (!GetWorld()) { return; }
-	if (GroundPoints.IsValidIndex(LocalPointsIndex))
+	if (GroundPoints.IsValidIndex(LocalPointsIndex) && GroundPoints.Num() > 1)
 	{
-		FGameplayCueParameters Parameters{};
-		Parameters.Location = GroundPoints[LocalPointsIndex++];
-		K2_ExecuteGameplayCueWithParams(FRPGAuraGameplayTags::Get().GameplayCue_Spawn_ArcaneShards, Parameters);
+		const auto CurrentCenter = GroundPoints[LocalPointsIndex++];
+
+		if (BShowDebugDraw)
+		{
+			UKismetSystemLibrary::DrawDebugSphere(this, CurrentCenter, ArcaneOutRadius, 1,
+			                                      FLinearColor::Black, 1,
+			                                      2);
+			UKismetSystemLibrary::DrawDebugSphere(this, CurrentCenter, ArcaneInnerRadius, 1,
+			                                      FLinearColor::White, 1,
+			                                      3);
+		}
+
+		CauseArcaneRadiusDamage(CurrentCenter);
 	}
 	else
 	{
-		GetWorld()->GetTimerManager().PauseTimer(ArcaneTimeHandle);
+		K2_CommitAbility();
 		K2_EndAbility();
 	}
 }
+
+void UBaseArcane::CauseArcaneRadiusDamage(const FVector& CurrentCenter)
+{
+	TArray<AActor*> LocalOutOverlappingActors{};
+	URPGAuraBlueprintFunctionLibrary::FindLivePlayersWithinRadius(GetAvatarActorFromActorInfo(),
+	                                                              LocalOutOverlappingActors, {}, ArcaneOutRadius,
+	                                                              CurrentCenter
+	                                                              , true,
+	                                                              FRPGAuraGameplayTags::Get().Player);
+	FGameplayCueParameters Parameters{};
+	Parameters.Location = CurrentCenter;
+	K2_ExecuteGameplayCueWithParams(FRPGAuraGameplayTags::Get().GameplayCue_Spawn_ArcaneShards, Parameters);
+
+	for (const auto& Actor : LocalOutOverlappingActors)
+	{
+		FDamageEffectParams DamageEffectParams{};
+		// 设置范围伤害缩减系数
+		DamageEffectParams.RadiusDamageFallOffFactor =
+			URPGAuraBlueprintFunctionLibrary::GetRadialDamageWithFallOffFactor(
+				Actor->GetActorLocation(), CurrentCenter, ArcaneOutRadius, ArcaneInnerRadius,
+				ArcaneOutRadiusMinDamageFactor, ArcaneOutRadiusTolerance);
+		// 设置冲击方向 默认从中心点向Actor冲击
+		const auto Dir = (Actor->GetActorLocation() - CurrentCenter).GetSafeNormal();
+		DamageEffectParams.ImpulseVector = Dir.RotateAngleAxis(45, Dir.RightVector) * KnockBackFactor *
+			KnockBackDirection;
+
+		CauseDamage(Actor, DamageEffectParams);
+		UKismetSystemLibrary::DrawDebugArrow(this, CurrentCenter,
+												 CurrentCenter.GetSafeNormal() + DamageEffectParams.ImpulseVector, 1000,
+												 FLinearColor::White, 3, 2);
+		if (BShowDebugDraw)
+		{
+			
+			UKismetSystemLibrary::DrawDebugSphere(this, Actor->GetActorLocation(), 20, 20,
+			                                      FLinearColor::Yellow, 1,
+			                                      3);
+			UE_LOG(UBaseArcaneLog, Warning,
+			       TEXT("Actor->[%s] , "
+				       "位置:[%s] , \n"
+				       "检测半径[%3.1f] , "
+				       "离圆心距离[%.3f] , "
+				       "容差距离[%.3f] , "
+				       "衰减系数:[%.3f] , "
+				       "最终伤害:[%.3f]"),
+			       *Actor->GetName(),
+			       *Actor->GetActorLocation().ToString(),
+			       ArcaneOutRadius,
+			       FVector::Dist(Actor->GetActorLocation(),CurrentCenter),
+			       ArcaneOutRadiusTolerance,
+			       DamageEffectParams.RadiusDamageFallOffFactor,
+			       DamageEffectParams.BaseDamage);
+		}
+	}
+}
+
+int32 UBaseArcane::GetValidAbilityCount(const int32 AbilityLevel) { 
+	return FMath::Min(AbilityLevel, ArcaneSpawnNum);
+ }
--- Source/RPGAura/Public/GAS/GameplayAbilities/BaseArcane.h
+++ Source/RPGAura/Public/GAS/GameplayAbilities/BaseArcane.h
@@ -16,15 +16,12 @@ class RPGAURA_API UBaseArcane : public UBaseDamageAbility
 	GENERATED_BODY()
 
 public:
-	/// 
-	/// @param StartCenter 
-	/// @param Radius 
-	/// @param Spread 
-	/// @param Points 
+	/// 检测地面上的点,用于生成奥术法术 
+	/// @param StartCenter    
 	UFUNCTION(BlueprintCallable)
-	void DetectGroundPoints(const FVector& StartCenter, const float Radius, const float Spread, const int32 Points);
+	void DetectGroundPoints(const FVector& StartCenter);
 
-	/// 生成奥术碎片
+	/// 生成奥术碎片,完成之后会自动结束能力
 	/// @param SpawnCenter 
 	UFUNCTION(BlueprintCallable)
 	void SpawnArcaneShards(const FVector& SpawnCenter);
@@ -37,19 +34,63 @@ public:
 	                        const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility,
 	                        bool bWasCancelled) override;
 
+
 protected:
-	/// 
+	/// 由Arcane定时器调用的回调函数
 	UFUNCTION()
 	void SpawnArcaneWithDelay();
+
+	/// 显示调式信息
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	bool BShowDebugDraw = false;
+
+	// 奥术法术的半径
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	float ArcaneOutRadius = 210.f;
+
+	// 奥术法术的内半径
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	float ArcaneInnerRadius = 125.f;
+
+	// 奥术法术的最大半径的容差, eg: 外半径为200,容差为30,那么当Actor距圆心230时仍然能受到伤害 (不是一定会生效,因为前提是要被范围检测到)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane", meta=(ClampMin=10.f, ClampMax=100.f))
+	float ArcaneOutRadiusTolerance = 30.f;
+
+	// 奥术法术的分散角度(以冲击点为圆心)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	float ArcaneSpread = 360.f;
+
+	// 奥术法术的生成数量 (由技能等级决定)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	int32 ArcaneSpawnNum = 5.f;
+
+	// 奥术法术生成延迟秒数(如果有多个)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane", meta=(ClampMin=0.1f, ClampMax=1.0f))
+	float ArcaneSpawnDelay = .2f;
+
+	// 刚好处于外圆时的最低伤害衰减系数
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Arcane", meta=(ClampMin=0.2f, ClampMax=1.0f))
+	float ArcaneOutRadiusMinDamageFactor = .5f;
+
+	/// 鼠标指针的碰撞点(在蓝图中设置)
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Arcane")
+	FVector CursorImpactPoint{};
+
+	/// 以地面坐标数组中的点为指定中心半径生成奥术碎片并且给检测到的Actor应用GE
+	/// @param CurrentCenter 
+	virtual void CauseArcaneRadiusDamage(const FVector& CurrentCenter);
+
+	virtual int32 GetValidAbilityCount(const int32 AbilityLevel) override;
 
 private:
 	UPROPERTY()
 	int32 LocalPointsIndex = 0;
-	
+
 	UPROPERTY()
 	TArray<FVector> GroundPoints;
 
 	// 用于生成多个奥术特效的定时器
 	UPROPERTY()
 	FTimerHandle ArcaneTimeHandle;
+	
 };
