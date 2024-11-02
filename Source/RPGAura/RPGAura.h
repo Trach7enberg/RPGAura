2d8cf82eef72cdc2f24141bdc9f323217ad8c8c0
my-laptop <2655667253@qq.com>
2024-11-02 23:35:00 +0800

    添加:RPGAura.h头文件新增对自定义追踪频道的宏别名

--- Source/RPGAura/RPGAura.h
+++ Source/RPGAura/RPGAura.h
@@ -8,4 +8,5 @@
 #define COLLISION_PRESET_PROJECTILE FName("Projectile")						// 碰撞预设 射弹
 #define COLLISION_PRESET_DETECT_GROUND_POINTS FName("DetectGroundPoints")	// 碰撞预设 用于检测地面上的点
 
-#define TRACE_CHANNEL_MAGICDECAL ECollisionChannel::ECC_GameTraceChannel3
+#define OBJECT_CHANNEL_MAGICDECAL ECollisionChannel::ECC_GameTraceChannel3	// 用于魔法贴花显示
+#define OBJECT_CHANNEL_PROJECTILE ECollisionChannel::ECC_GameTraceChannel1	// 用于飞弹
