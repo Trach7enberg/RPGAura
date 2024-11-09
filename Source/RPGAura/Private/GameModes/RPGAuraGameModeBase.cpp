6fb823f2396159ae4f468e6dfb1589dee3e639dd
my-laptop <2655667253@qq.com>
2024-11-09 23:38:09 +0800

    添加:基础游戏模式新增用于持久化数据到磁盘的相应方法,以及新增一个集合用于存储关卡地图

--- Source/RPGAura/Private/GameModes/RPGAuraGameModeBase.cpp
+++ Source/RPGAura/Private/GameModes/RPGAuraGameModeBase.cpp
@@ -5,11 +5,72 @@
 
 #include "Characters/AuraCharacter.h"
 #include "Controller/BasePlayerController.h"
+#include "CoreTypes/RPGAuraGameplayTags.h"
+#include "Kismet/GameplayStatics.h"
+#include "Save/LoadScreen/LoadScreenSave.h"
+#include "UI/MVVM/ViewModel/MVVMLoadSlotSwitcher.h"
 
-DEFINE_LOG_CATEGORY_STATIC(ARPGAuraGameModeBaseLog,All,All);
+DEFINE_LOG_CATEGORY_STATIC(ARPGAuraGameModeBaseLog, All, All);
 
 ARPGAuraGameModeBase::ARPGAuraGameModeBase()
 {
 	DefaultPawnClass = AAuraCharacter::StaticClass();
 	PlayerControllerClass = ABasePlayerController::StaticClass();
 }
+
+void ARPGAuraGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	const auto Level = LevelMaps.Find(StartingMapName);
+	if (!Level)
+	{
+		UE_LOG(ARPGAuraGameModeBaseLog, Error, TEXT("[%s]无法找到关卡!"), *FString::Printf(TEXT(__FUNCTION__)));
+	}
+}
+
+
+void ARPGAuraGameModeBase::SaveLoadSlotData(UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel, const int32 SaveSlotIndex)
+{
+	if (!LoadSlotSwitcherViewModel) { return; }
+	
+	// 要保持的存档如果已经存在,则删除
+	DeleteSlotData(LoadSlotSwitcherViewModel);
+
+	auto SaveGameObj = UGameplayStatics::CreateSaveGameObject(ULoadScreenSave::StaticClass());
+	if (!SaveGameObj) { return; }
+
+	auto LoadScreenSaveGameObj = Cast<ULoadScreenSave>(SaveGameObj);
+	if (!LoadScreenSaveGameObj) { return; }
+
+	LoadScreenSaveGameObj->PlayerName = LoadSlotSwitcherViewModel->GetPlayerName();
+	LoadScreenSaveGameObj->CurrentLoadSlotSwitcherLoadSlotTag = FRPGAuraGameplayTags::Get().Widget_Menu_Load_Slot_Taken;
+	UGameplayStatics::SaveGameToSlot(LoadScreenSaveGameObj, LoadSlotSwitcherViewModel->GetLoadSlotName(),
+	                                 SaveSlotIndex);
+}
+
+ULoadScreenSave* ARPGAuraGameModeBase::GetLoadSlotData(const FString& Name, const int32 SlotIndex) const
+{
+	return Cast<ULoadScreenSave>(UGameplayStatics::LoadGameFromSlot(Name, SlotIndex));
+}
+
+ULoadScreenSave* ARPGAuraGameModeBase::GetLoadSlotData(const UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel) const
+{
+	if (!UGameplayStatics::DoesSaveGameExist(LoadSlotSwitcherViewModel->GetLoadSlotName(),
+	                                         LoadSlotSwitcherViewModel->GetLoadSlotInSave())) { return nullptr; }
+	return Cast<ULoadScreenSave>(UGameplayStatics::LoadGameFromSlot(LoadSlotSwitcherViewModel->GetLoadSlotName(),
+	                                                                LoadSlotSwitcherViewModel->GetLoadSlotInSave()));
+}
+
+bool ARPGAuraGameModeBase::DeleteSlotData(const UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel)
+{
+	if (!IsValid(LoadSlotSwitcherViewModel)) { return false; }
+
+	// 要保持的存档已经存在,则删除
+	if (UGameplayStatics::DoesSaveGameExist(LoadSlotSwitcherViewModel->GetLoadSlotName(), LoadSlotSwitcherViewModel->GetLoadSlotInSave()))
+	{
+		return UGameplayStatics::DeleteGameInSlot(LoadSlotSwitcherViewModel->GetLoadSlotName(), LoadSlotSwitcherViewModel->GetLoadSlotInSave());
+	}
+
+	return false;
+}
+
--- Source/RPGAura/Public/GameModes/RPGAuraGameModeBase.h
+++ Source/RPGAura/Public/GameModes/RPGAuraGameModeBase.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "RPGAuraGameModeBase.generated.h"
 
+class ULoadScreenSave;
+class UMVVMLoadSlotSwitcher;
 class UTagToAbilityInfoAsset;
 /**
  * 游戏模式类
@@ -16,5 +18,36 @@ class RPGAURA_API ARPGAuraGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 	ARPGAuraGameModeBase();
 
+public:
+	/// 保存数据到指定的槽位
+	/// @param LoadSlotSwitcherViewModel  
+	/// @param SaveSlotIndex 要保存的槽位
+	void SaveLoadSlotData(UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel,int32 SaveSlotIndex);
 
+	/// 根据存档的名字从指定槽位加载数据
+	/// @param Name 存档的名字(一般为玩家的名字)
+	/// @param SlotIndex 
+	/// @return 
+	ULoadScreenSave* GetLoadSlotData(const FString& Name,int32 SlotIndex) const;
+
+	/// 根据存档的名字从指定槽位加载数据
+	/// @param LoadSlotSwitcherViewModel 
+	/// @return 
+	ULoadScreenSave* GetLoadSlotData(const UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel) const;
+
+	/// 删除指定槽位的存档
+	/// @param LoadSlotSwitcherViewModel
+	bool DeleteSlotData(const UMVVMLoadSlotSwitcher* LoadSlotSwitcherViewModel);
+
+protected:
+
+	/// 初始地图,请在Map中选择
+	UPROPERTY(EditDefaultsOnly,Category="Level")
+	FString StartingMapName{};
+	
+	// 关卡
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Level")
+	TMap<FString,TSoftObjectPtr<UWorld>> LevelMaps{};
+
+	virtual void BeginPlay() override;
 };
