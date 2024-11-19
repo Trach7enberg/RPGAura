15eeaf927af5a01056ff8cae959ed87f48d004b1
my-laptop <2655667253@qq.com>
2024-11-19 08:45:13 +0800

    添加:ACheckPointPlayerStart基础检查点检查点PlayerStart实现两个接口public ISaveInterface,public IHighLightInterface

--- Source/RPGAura/Private/PlayerStart/CheckPointPlayerStart.cpp
+++ Source/RPGAura/Private/PlayerStart/CheckPointPlayerStart.cpp
@@ -5,7 +5,10 @@
 
 #include "Components/BoxComponent.h"
 #include "CoreTypes/RPGAuraGameplayTags.h"
+#include "GameModes/RPGAuraGameModeBase.h"
 #include "Interfaces/PlayerInterface.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMaterialLibrary.h"
 
 DEFINE_LOG_CATEGORY_STATIC(ACheckPointPlayerStartLog, All, All);
 
@@ -15,9 +18,11 @@ ACheckPointPlayerStart::ACheckPointPlayerStart(const FObjectInitializer& ObjectI
 
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
 	BoxCollision        = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	AutoMovePosition         = CreateDefaultSubobject<USceneComponent>("AutoMovePos");
 
 	StaticMeshComponent->SetupAttachment(GetRootComponent());
 	BoxCollision->SetupAttachment(GetRootComponent());
+	AutoMovePosition->SetupAttachment(GetRootComponent());
 
 	BoxCollision->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
 	BoxCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
@@ -28,10 +33,55 @@ ACheckPointPlayerStart::ACheckPointPlayerStart(const FObjectInitializer& ObjectI
 	StaticMeshComponent->SetCollisionResponseToAllChannels(ECR_Block);
 }
 
+bool ACheckPointPlayerStart::ShouldLoadTransform()
+{
+	// 检查点是固定的因此不需要更新变换
+	return false;
+}
+
+void ACheckPointPlayerStart::LoadActorSaveInfo()
+{
+	// 从磁盘加载时,是否保持发光(如果序列化到磁盘时是发光的状态,那么加载时就会保持发光)?
+	if (Reached)
+	{
+		BoxCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		HandelGlowMaterial();
+		CheckPointReached();
+	}
+}
+
+void ACheckPointPlayerStart::HighLightActor()
+{
+	if (!StaticMeshComponent) { return; }
+	StaticMeshComponent->SetRenderCustomDepth(true);
+	StaticMeshComponent->SetCustomDepthStencilValue(CustomDepthStencilValue);
+}
+
+void ACheckPointPlayerStart::UnHighLightActor()
+{
+	if (!StaticMeshComponent) { return; }
+	StaticMeshComponent->SetRenderCustomDepth(false);
+	StaticMeshComponent->SetCustomDepthStencilValue(0);
+}
+
+void ACheckPointPlayerStart::GetMoveToLocation(FVector& OutDestination)
+{
+	OutDestination = AutoMovePosition->GetComponentLocation();
+}
+
 void ACheckPointPlayerStart::BeginPlay()
 {
 	Super::BeginPlay();
 	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ACheckPointPlayerStart::OnSphereOverlap);
+	StaticMeshComponent->OnBeginCursorOver.AddDynamic(this, &ACheckPointPlayerStart::OnCursorBegin);
+	StaticMeshComponent->OnEndCursorOver.AddDynamic(this, &ACheckPointPlayerStart::OnCursorEnd);
+}
+
+void ACheckPointPlayerStart::HandelGlowMaterial()
+{
+	if(LocalCheckPointDynamicMi){return;}
+	LocalCheckPointDynamicMi = UKismetMaterialLibrary::CreateDynamicMaterialInstance(this, CheckPointMi);
+	StaticMeshComponent->SetMaterial(0, LocalCheckPointDynamicMi);
 }
 
 void ACheckPointPlayerStart::OnSphereOverlap(
@@ -42,12 +92,13 @@ void ACheckPointPlayerStart::OnSphereOverlap(
 	bool                 bFromSweep,
 	const FHitResult&    SweepResult)
 {
-	if (!OtherActor || !OtherActor->ActorHasTag(FRPGAuraGameplayTags::Get().Player)) { return; }
+	if (!OtherActor || !OtherActor->ActorHasTag(FRPGAuraGameplayTags::Get().Player) || !CheckPointMi) { return; }
 
 	const auto PlayerIntF = Cast<IPlayerInterface>(OtherActor);
 	if (!PlayerIntF) { return; }
 
 	BoxCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	HandelGlowMaterial();
 	CheckPointReached();
 
 	if (!PlayerStartTag.IsValid() || PlayerStartTag.IsNone())
@@ -55,11 +106,25 @@ void ACheckPointPlayerStart::OnSphereOverlap(
 		UE_LOG(ACheckPointPlayerStartLog, Error, TEXT("[%s]检查点无效!!"), TEXT(__FUNCTION__));
 		return;
 	}
+	Reached = true;
 	PlayerIntF->SaveProgress(PlayerStartTag);
+
+	if (const auto MyGameMode = Cast<ARPGAuraGameModeBase>(UGameplayStatics::GetGameMode(this)))
+		
+	{
+		auto MapAssetName = GetWorld()->GetMapName();
+		MapAssetName.RemoveFromStart(GetWorld()->StreamingLevelsPrefix);
+		MyGameMode->SaveWorldState(GetWorld(),MapAssetName);
+	}
+	else
+	{
+		UE_LOG(ACheckPointPlayerStartLog,
+		       Error,
+		       TEXT("[%s]检查点 %s:无法保存世界状态!!"),
+		       TEXT(__FUNCTION__),
+		       *PlayerStartTag.ToString());
+	}
 }
 
-
-void ACheckPointPlayerStart::HandleMeshMaterial()
-{
-	// BoxCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-}
+void ACheckPointPlayerStart::OnCursorBegin(UPrimitiveComponent* TouchedComponent) { HighLightActor(); }
+void ACheckPointPlayerStart::OnCursorEnd(UPrimitiveComponent* TouchedComponent) { UnHighLightActor(); }
--- Source/RPGAura/Public/PlayerStart/CheckPointPlayerStart.h
+++ Source/RPGAura/Public/PlayerStart/CheckPointPlayerStart.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerStart.h"
+#include "Interfaces/HighLightInterface.h"
+#include "Interfaces/SaveInterface.h"
 #include "CheckPointPlayerStart.generated.h"
 
 class UBoxComponent;
@@ -11,7 +13,7 @@ class UBoxComponent;
  *  检查点PlayerStart
  */
 UCLASS()
-class RPGAURA_API ACheckPointPlayerStart : public APlayerStart
+class RPGAURA_API ACheckPointPlayerStart : public APlayerStart, public ISaveInterface,public IHighLightInterface
 {
 	GENERATED_BODY()
 
@@ -19,25 +21,65 @@ public:
 	ACheckPointPlayerStart(const FObjectInitializer& ObjectInitializer);
 
 	/// 角色到达检查点时候的事件
-	UFUNCTION(BlueprintCallable,BlueprintImplementableEvent)
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
 	void CheckPointReached();
+
+	/// 当前检查点是否已经到达 (带有SaveGame标识很重要!)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, SaveGame)
+	bool Reached = false;
+
+	// ~SaveInterface
+	virtual bool ShouldLoadTransform() override;
+	virtual void LoadActorSaveInfo() override;
 	
+	// ~HighLightInterface
+	UFUNCTION(BlueprintCallable)
+	virtual void HighLightActor() override;
+	UFUNCTION(BlueprintCallable)
+	virtual void UnHighLightActor() override;
+
+	virtual void GetMoveToLocation(FVector& OutDestination) override;
+	// ~HighLightInterface
+
 protected:
-	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite, Category="Level")
+	/// Mesh的描边颜色
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
+	float CustomDepthStencilValue = 252.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
 	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
 
-	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,  Category="Level")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
 	TObjectPtr<UBoxComponent> BoxCollision;
 
-	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,  Category="Level")
+	/// 标记自动行走位置的箭头组件
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
+	TObjectPtr<USceneComponent> AutoMovePosition;
+
+	/// 检查点的发光材质
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
 	TObjectPtr<UMaterialInterface> CheckPointMi;
+
+	/// 给Mesh设置的动态材质,用于更改发光
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Level")
+	TObjectPtr<UMaterialInstanceDynamic> LocalCheckPointDynamicMi;
 
 	virtual void BeginPlay() override;
 
-	void HandleMeshMaterial();
-	
+	virtual void HandelGlowMaterial();
+
 	UFUNCTION()
-	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-	                             const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor*              OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32                OtherBodyIndex,
+		bool                 bFromSweep,
+		const FHitResult&    SweepResult);
+
+	UFUNCTION()
+	virtual void OnCursorBegin(UPrimitiveComponent* TouchedComponent);
+
+	UFUNCTION()
+	virtual void OnCursorEnd(UPrimitiveComponent* TouchedComponent);
 };
