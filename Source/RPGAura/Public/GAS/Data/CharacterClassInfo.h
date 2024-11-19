484dba6f710c321b89351d047fbc345c4ad4403b
my-laptop <2655667253@qq.com>
2024-11-19 08:36:39 +0800

    添加:UCharacterClassInfo数据资产类新增属性初始化GE,PrimaryAttributesFromDiskSetByCaller,主要是角色的主要GE属性(通过磁盘加载然后设置SetByCaller)

--- Source/RPGAura/Public/GAS/Data/CharacterClassInfo.h
+++ Source/RPGAura/Public/GAS/Data/CharacterClassInfo.h
@@ -19,21 +19,23 @@ class RPGAURA_API UCharacterClassInfo : public UDataAsset
 
 public:
 	// 角色类别映射到对应的角色默认信息结构Map
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Character Class Defaults")
-	TMap<ECharacterClass,FCharacterClassDefaultInfo> CharacterClassInformation;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Character Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	// 角色的主要GE属性(通过磁盘加载然后设置SetByCaller)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "DefaultAttributes|Primary")
+	TSubclassOf<UGameplayEffect> PrimaryAttributesFromDiskSetByCaller;
 
 	// 角色(敌人)的共同属性(Secondary)影响初始化(注意,当前敌人的次要属性不会受到主属性的升级影响,即只是Instant,不是无限效果)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> SecondaryAttributesEnemy;
 
 	// 角色(玩家)的共同属性(Secondary)影响初始化(注意,当前玩家的次要属性会受到主属性影响)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> SecondaryAttributesPlayer;
 
-	
-
 	// 角色的共同属性(Vital)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,Category="Common Class Defaults|GameplayEffects")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|GameplayEffects")
 	TSubclassOf<UGameplayEffect> VitalAttributes;
 
 	// 角色共同拥有的能力
@@ -43,9 +45,9 @@ public:
 	// 角色的伤害计算系数表
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Common Class Defaults|CurveTables")
 	TObjectPtr<UCurveTable> DamageCalculationFactors;
-	
+
 	/// 根据角色职业枚举查找对应角色的默认职业数据
 	/// @param CharacterClass 角色职业枚举
 	/// @return 角色的默认职业数据结构
-	FCharacterClassDefaultInfo FindClassDefaultInfo(const ECharacterClass & CharacterClass);
+	FCharacterClassDefaultInfo FindClassDefaultInfo(const ECharacterClass& CharacterClass);
 };
