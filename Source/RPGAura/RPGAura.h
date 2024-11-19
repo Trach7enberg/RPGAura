c2e52bcbb0b12cba34b3f898d03727e10bd96452
my-laptop <2655667253@qq.com>
2024-11-19 08:45:59 +0800

    添加:RPGAura.h新增3个自定义宏用于深度模板的颜色

--- Source/RPGAura/RPGAura.h
+++ Source/RPGAura/RPGAura.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 
-#define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_RED 250	// 角色描边颜色-红色
+#define CUSTOM_DEPTH_BLUE 251	// 角色描边颜色-蓝色
+#define CUSTOM_DEPTH_TAN 252	// 角色描边颜色-棕褐色
 #define COLLISION_PRESET_PROJECTILE FName("Projectile")						// 碰撞预设 射弹
 #define COLLISION_PRESET_DETECT_GROUND_POINTS FName("DetectGroundPoints")	// 碰撞预设 用于检测地面上的点
 
